9a1e8768c4a641eee417d6c3a031925dce8372d3
sumin <sumin50240585@gmail.com>
2025-03-30 23:23:45 +0900

    🔧 chore :  Tailwind CSS ver. 4 업데이트

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,7 @@
 import {NextPage} from "next";
 
 const Login: NextPage =() => (
-<div className="text-3xl font bold p-10">
+<div className="text-3xl font bold underline">
     <h1 className="text-pink-500">소셜 로그인 창 - test</h1>
 </div>
 )
