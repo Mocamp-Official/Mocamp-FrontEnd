61da382eff0589c4968ad1bf8c0637b406f7e1ef
LimJaeHyun <guswogus81@gmail.com>
2025-04-05 15:01:26 +0900

    🔧 chore: vercel 배포를 위한 에러 수정

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,1 +1,6 @@
 /* 모캠프(방) 생성 페이지 */
+import React from 'react';
+
+export default function create() {
+  return <div>create</div>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,6 @@
 /* 로그인 페이지 리다이렉션 시킴*/
+import React from 'react';
+
+export default function Redirection() {
+  return <div>index</div>;
+}
