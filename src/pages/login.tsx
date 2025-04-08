501b3ad0f564d861524dad0ace3bc18146d6c725
LimJaeHyun <guswogus81@gmail.com>
2025-04-08 23:21:14 +0900

    ⬆️ update : 로그인 페이지 퍼블리싱

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,22 @@
-/* 로그인 페이지*/
-import {NextPage} from "next";
+import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
+import KakaoLoginButton from '@/components/auth/KakaoLoginButton';
+import NaverLoginButton from '@/components/auth/NaverLoginButton';
+import MocampIcon from '@/public/svgs/MocampIcon.svg';
 
-const Login: NextPage =() => (
-<div className="text-3xl font bold underline">
-    <h1 className="text-pink-500">소셜 로그인 창 - test</h1>
-</div>
-)
-
-export default Login;
+export default function LoginPage() {
+  return (
+    <div className="flex bg-[#F2F2F2] w-screen h-screen">
+      <div className="w-[660px] h-100% mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
+        <MocampIcon />
+        <p className="my-10 text-2xl">
+          함께 모여 캠키고 성취하는 우리만의 공간
+        </p>
+        <div className="flex flex-col gap-3">
+          <NaverLoginButton></NaverLoginButton>
+          <KakaoLoginButton></KakaoLoginButton>
+          <GoogleLoginButton></GoogleLoginButton>
+        </div>
+      </div>
+    </div>
+  );
+}
