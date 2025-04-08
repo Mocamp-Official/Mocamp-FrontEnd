5d170a0f1abea0a863d9ab818dbdb4bbaf20a7c8
LimJaeHyun <guswogus81@gmail.com>
2025-04-09 00:06:16 +0900

    ♻️ refactor: 호버 추가 & 일부 CSS 변경

--- src/components/auth/GoogleLoginButton.tsx
+++ src/components/auth/GoogleLoginButton.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 export default function GoogleLoginButton() {
   return (
-    <button className="flex bg-[#FFFFFF] w-[450px] max-w-md h-16 justify-center items-center space-x-4 rounded-[5px] relative border border-[#D4D4D4]">
-      <div className="absolute left-[30px] w-6 h-6 bg-[#a3a3a3]">
-        {/* 로고로 변경 예정 (네이버) */}
+    <button className="flex bg-white w-full max-w-md h-16 justify-center items-center relative border border-gray-300 group">
+      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 rounded pointer-events-none transition-opacity duration-200"></div>
+      <div className="absolute left-8 w-6 h-6 bg-gray-400">
+        {/* 로고로 변경 예정 (구글) */}
       </div>
-      <p className="text-[#4B4B4B] text-lg font-semibold">구글로 시작하기</p>
+      <p className="text-gray-700 text-lg font-semibold">구글로 시작하기</p>
     </button>
   );
 }
--- src/components/auth/NaverLoginButton.tsx
+++ src/components/auth/NaverLoginButton.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 export default function NaverLoginButton() {
   return (
-    <button className="flex bg-[#05C050] w-[450px] max-w-md h-16 justify-center items-center space-x-4 rounded-[5px] relative">
-      <div className="absolute left-[30px] w-6 h-6 bg-[#a3a3a3]">
-        {/* 로고로 변경 예정 (네이버) */}
+    <button className="flex bg-[#05C050] w-full max-w-md h-16 justify-center items-center relative border border-gray-300 rounded-[5px] group">
+      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 rounded-[5px] pointer-events-none transition-opacity duration-200"></div>
+      <div className="absolute left-8 w-6 h-6 bg-gray-400">
+        {/* 로고로 변경 예정 (구글) */}
       </div>
-      <p className="text-white text-lg font-semibold">네이버로 시작하기</p>
+      <p className="text-[#ffffff] text-lg font-semibold">네이버로 시작하기</p>
     </button>
   );
 }
