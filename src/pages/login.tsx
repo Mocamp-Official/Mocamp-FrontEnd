113da684ec72ccd79f8555db6ca3de48c237ea74
LimJaeHyun <guswogus81@gmail.com>
2025-04-09 00:06:46 +0900

    ♻️ refactor: 일부 CSS 수정

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,10 +8,10 @@ export default function LoginPage() {
     <div className="flex bg-[#F2F2F2] w-screen h-screen">
       <div className="w-[660px] h-100% mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
         <MocampIcon />
-        <p className="my-10 text-2xl">
+        <p className="my-10 text-2xl font-medium">
           함께 모여 캠키고 성취하는 우리만의 공간
         </p>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 w-[450px]">
           <NaverLoginButton></NaverLoginButton>
           <KakaoLoginButton></KakaoLoginButton>
           <GoogleLoginButton></GoogleLoginButton>
