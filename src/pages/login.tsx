b85a2a1924c42f74b63b3926f6903ef1c8d366bf
Congee <guswogus81@gmail.com>
2025-05-05 20:15:14 +0900

    ✨ feat: 로그인 페이지 UI 구현 (#7)
    * 🔧 chore: svgr & next-auth 라이브러리 추가
    
    * 🔧 chore: public 파일도 @ import 사용 가능하도록 수정
    
    * ✨ feat: 구글, 카카오, 네이버 버튼
    
    * ✨ feat: 모캠프 사진 추가
    
    * ⬆️ update : 로그인 페이지 퍼블리싱
    
    * ♻️ refactor: 호버 추가 & 일부 CSS 변경
    
    * ⬆️ update : 추가
    
    * ♻️ refactor: 일부 CSS 수정
    
    * ♻️ refactor: 변화된 UI 적용 & 로그인 버튼의 최근 로그인 UI 추가
    
    * ♻️ refactor: 배경 색상 변경 & 텍스트 변경
    
    * ♻️ refactor: 모캠프 로고 변경
    
    * ♻️ refactor: "/" 페이지 요청 시 "/login" 페이지로 리다렉트 되는 설정 삭제
    
    * ♻️ refactor: 코드 컨벤션에 맞춰 코드 수정

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,29 @@
-/* 로그인 페이지*/
-import {NextPage} from "next";
+import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
+import KakaoLoginButton from '@/components/auth/KakaoLoginButton';
+import NaverLoginButton from '@/components/auth/NaverLoginButton';
+import MocampIcon from '@/public/svgs/MocampIcon.svg';
 
-const Login: NextPage =() => (
-<div className="text-3xl font bold underline">
-    <h1 className="text-pink-500">소셜 로그인 창 - test</h1>
-</div>
-)
+const LoginPage = () => {
+  return (
+    <div className="flex bg-[#ffffff] w-screen h-screen">
+      <div className="h-100% mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
+        <MocampIcon />
+        <div className="mt-8 mb-32 text-2xl font-medium">
+          <p>
+            모캠프는{' '}
+            <span className="text-[#27CFA5] font-semibold">로그인</span> 후
+          </p>
+          <p className="mt-2">시작할 수 있어요 :D</p>
+        </div>
 
-export default Login;
+        <div className="flex flex-col gap-5 w-[600px]">
+          <NaverLoginButton />
+          <KakaoLoginButton />
+          <GoogleLoginButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
