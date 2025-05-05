abcceb94efcb69ae54a4a1433a057bb9127d0a20
LimJaeHyun <guswogus81@gmail.com>
2025-05-05 18:13:24 +0900

    ♻️ refactor: 변화된 UI 적용 & 로그인 버튼의 최근 로그인 UI 추가

--- src/components/auth/GoogleLoginButton.tsx
+++ src/components/auth/GoogleLoginButton.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 export default function GoogleLoginButton() {
   return (
-    <button className="flex bg-white w-full max-w-md h-16 justify-center items-center relative border border-gray-300 group">
+    <button className="flex bg-white w-full h-16 justify-center items-center relative border border-gray-300 group">
       <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 rounded pointer-events-none transition-opacity duration-200"></div>
-      <div className="absolute left-8 w-6 h-6 bg-gray-400">
-        {/* 로고로 변경 예정 (구글) */}
+      {/* <div className="absolute left-8 w-6 h-6 bg-gray-400">로고로 변경 예정 (구글)</div> */}
+      <p className="text-gray-700 text-lg font-semibold">
+        구글 계정으로 시작하기
+      </p>
+      <div className="absolute right-[75px] top-[-26px] items-center justify-center px-5 py-2.5 rounded-tl-[10px] rounded-tr-[10px] rounded-br-[10px] bg-white border border-[#00af83]">
+        <p className="flex w-22.5 h-8 text-xl justify-center items-center font-semibold text-[#00af83] text-center">
+          최근 로그인
+        </p>
       </div>
-      <p className="text-gray-700 text-lg font-semibold">구글로 시작하기</p>
     </button>
   );
 }
--- src/components/auth/NaverLoginButton.tsx
+++ src/components/auth/NaverLoginButton.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 export default function NaverLoginButton() {
   return (
-    <button className="flex bg-[#05C050] w-full max-w-md h-16 justify-center items-center relative border border-gray-300 rounded-[5px] group">
+    <button className="flex bg-[#05C050] w-full h-16 justify-center items-center relative border border-gray-300 rounded-[5px] group">
       <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 rounded-[5px] pointer-events-none transition-opacity duration-200"></div>
-      <div className="absolute left-8 w-6 h-6 bg-gray-400">
-        {/* 로고로 변경 예정 (구글) */}
+      {/* <div className="absolute left-8 w-6 h-6 bg-gray-400">로고로 변경 예정 (구글)</div> */}
+      <p className="text-[#ffffff] text-lg font-semibold">
+        네이버 계정으로 시작하기
+      </p>
+      <div className="absolute right-[75px] top-[-26px] items-center justify-center px-5 py-2.5 rounded-tl-[10px] rounded-tr-[10px] rounded-br-[10px] bg-white border border-[#00af83]">
+        <p className="flex w-22.5 h-8 text-xl justify-center items-center font-semibold text-[#00af83] text-center">
+          최근 로그인
+        </p>
       </div>
-      <p className="text-[#ffffff] text-lg font-semibold">네이버로 시작하기</p>
     </button>
   );
 }
