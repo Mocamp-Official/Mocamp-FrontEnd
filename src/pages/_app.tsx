af527acbfac989165871cca014e691a1fb2996bb
sumin <sumin50240585@gmail.com>
2025-05-07 14:32:41 +0900

    ⏪ rewind : 기본 구조로 되돌림림

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { useRouter } from 'next/router';
-import BasicHeader from '@/components/Header/BasicHeader';
-import OnboardingHeader from '@/components/Header/OnboardingHeader';
-import CreateJoinHeader from '@/components/Header/CreateJoinHeader';
-import WorkspaceHeader from '@/components/Header/WorkSpaceHeader';
-
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter(); 
-  const isLoginPage = router.pathname === '/login';
   return (
     <>
-      {isLoginPage && <OnboardingHeader/>}
       <Component {...pageProps} />
+      <SpeedInsights />
     </>
   );
 }
-
-
