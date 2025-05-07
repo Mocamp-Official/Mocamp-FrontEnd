47f659d56cb1aaa7ce90a86d644e88c262626f57
sumin <sumin50240585@gmail.com>
2025-05-07 13:40:13 +0900

    ⬆️ update: 모캠프 생성하기 & 모캠프 참여하기 버튼 클릭 시 모달창 open(추후 주석 해제 예정)

--- src/components/Header/CreateJoinHeader.tsx
+++ src/components/Header/CreateJoinHeader.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+//마이 홈 헤더
+import React, { useState } from 'react';
+//모달 개발 후 import
 
 const CreateJoinHeader = () => {
+    const [isCreateModalOpen, setCreateModalOpen] = useState(false);
+    const [isJoinModalOpen, setJoinModalOpen] = useState(false);
+
+    //모달 열기
+    const handleCreateClick = () => setCreateModalOpen(true);
+    const handleJoinClick = () => setJoinModalOpen(true)
+    
+    //모달 닫기
+    const closeModals = () => {
+        setCreateModalOpen(false);
+        setJoinModalOpen(false);
+    };
+
+
+
 return (
     <header className="w-full bg-white h-[100px] min-w-[1920px]">
     <div className="relative w-[1920px] h-full mx-auto">
         <img
-        src="/mocamp_logo.svg"
+        src="/svgs/MocampIcon.svg"
         alt="모캠프 로고"
         className="absolute top-[26px] left-[320px] w-[120px] h-[48.46px]"
         />
 
-        {/* 버튼(기존 디자인에서 패딩값, 폰트 크기 차이 존재*/}
         <button
         type="button"
+        // onClick={handleJoinClick}
         className="
         absolute top-[25px] left-[1248px]
         w-[171px] h-[51px]
         rounded-[10px]
         px-[20px] py-[10px]
         font-pre font-semibold
-        text-[17px] leading-[100%] tracking-[-0.02em] 
+        text-[18px] leading-[100%] tracking-[-0.02em] 
         text-[#555555]
         bg-gray-100
         hover:bg-gray-200
@@ -31,13 +48,14 @@ return (
 
         <button
         type="button"
+        // onClick={handleCreateClick}
         className="
         absolute top-[25px] left-[1429px]
         w-[171px] h-[51px]
         rounded-[10px]
         px-[20px] py-[10px]
         font-pre font-semibold
-        text-[17px] leading-[100%] tracking-[-0.02em]
+        text-[18px] leading-[100%] tracking-[-0.02em]
         text-white
         bg-[#27CFA5]
         hover:bg-teal-500
@@ -46,6 +64,13 @@ return (
         모캠프 생성하기
         </button>
     </div>
+    {/* 추 후 추가 */}
+    {/* {/*<Modal isOpen={isJoinModalOpen} onClose={closeModals} title="모캠프 참여하기">
+        <p>링크 복사 모달창</p></Modal> */}
+
+
+    {/* <Modal isOpen={isCreateModalOpen} onClose={closeModals} title="모캠프 생성하기기">
+        <p>방 생성 모달달</p> </Modal> */}
     </header>
 );
 };
