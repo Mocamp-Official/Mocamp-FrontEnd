dc5f8116f5b921a6205d77329d0636f79c5eb444
sumin <sumin50240585@gmail.com>
2025-05-07 21:20:55 +0900

    ⏪ rewind : 다시 파일 되돌리기기

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+/* 로그인 페이지*/
+//삭제했었으나 충돌이..계속 나서 살림림
+import {NextPage} from "next";
+
+const Login: NextPage =() => (
+<div className="text-3xl font bold underline">
+    <h1 className="text-pink-500">소셜 로그인 창 - test</h1>
+</div>
+)
+
+export default Login;
