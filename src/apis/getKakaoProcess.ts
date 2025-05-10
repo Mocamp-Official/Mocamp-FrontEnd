727939f77c29e4c3487ade2ccb36eb38da8f5cee
LimJaeHyun <guswogus81@gmail.com>
2025-05-10 17:45:52 +0900

    ♻️ refactor: 카카오 로그인 코드 수정

--- src/apis/getKakaoProcess.ts
+++ src/apis/getKakaoProcess.ts
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 const getKakaoProcess = async (code: string | null): Promise<boolean | undefined> => {
-  if (code === null) {
+  if (code === null || !code) {
     return false;
   }
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login/kakao/process`;
-  if (!code) return; // code가 없으면 실행하지 않음
   try {
-    const res = await axios.get(`${url}?code=${code}`);
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login/kakao/process?code=${code}`,
+    );
     const data = await res.data;
-    // console.log('데이터:', data);
+    console.log('카카오 로그인 데이터:', data);
     localStorage.setItem('accessToken', data.access_token);
     localStorage.setItem('refreshToken', data.refresh_token);
-    return await true;
+    return true;
   } catch (err) {
     alert(`카카오 로그인 에러 발생 : ${err}`);
-    throw Error('카카오 로그인 에러 발생');
+    return false;
   }
 };
 
