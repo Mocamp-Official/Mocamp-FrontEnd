b06d9685f521edda40dc88d90184fde73a1726ee
LimJaeHyun <guswogus81@gmail.com>
2025-05-10 19:07:21 +0900

    ⬆️ update : 최근 로그인 스타일 추가 & 최근 로그인 기능 구현

--- src/components/auth/GoogleLoginButton.tsx
+++ src/components/auth/GoogleLoginButton.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import axios from 'axios';
+import { platformType } from 'src/pages/login';
 
-const GoogleLoginButton = () => {
+interface NaverLoginButtonProps {
+  platform: platformType;
+}
+
+const GoogleLoginButton: React.FC<NaverLoginButtonProps> = ({ platform }) => {
   const handleGoogleLogin = async () => {
     try {
       const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login/google/page`);
       const data = res.data;
+      localStorage.setItem('platform', 'google'); // 플랫폼 저장
       window.location.href = data;
     } catch (err) {
       console.error(err);
@@ -22,7 +28,11 @@ const GoogleLoginButton = () => {
       <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 rounded pointer-events-none transition-opacity duration-200"></div>
       {/* <div className="absolute left-8 w-6 h-6 bg-gray-400">로고로 변경 예정 (구글)</div> */}
       <p className="text-gray-700 text-[28px] font-semibold">구글 계정으로 시작하기</p>
-      <div className="absolute right-[15px] top-[-26px] items-center justify-center px-5 py-2.5 rounded-tl-[10px] rounded-tr-[10px] rounded-br-[10px] bg-white border border-[#00af83]">
+      <div
+        className={`absolute right-[15px] top-[-26px] items-center justify-center px-5 py-2.5 rounded-tl-[10px] rounded-tr-[10px] rounded-br-[10px] bg-white border border-[#00af83] ${
+          platform === 'google' ? 'flex' : 'hidden'
+        }`}
+      >
         <p className="flex w-22.5 h-8 text-xl justify-center items-center font-semibold text-[#00af83] text-center">
           최근 로그인
         </p>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,56 +1,63 @@
 import { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { getGoogleProcess, getNaverProcess, getKakaoProcess } from '@/apis/auth';
+import MocampIcon from '@/public/svgs/MocampIcon.svg';
 import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
 import KakaoLoginButton from '@/components/auth/KakaoLoginButton';
 import NaverLoginButton from '@/components/auth/NaverLoginButton';
-import MocampIcon from '@/public/svgs/MocampIcon.svg';
-import { getGoogleProcess, getNaverProcess, getKakaoProcess } from '@/apis/auth';
 
-type platformType = 'naver' | 'kakao' | 'google' | null;
+export type platformType = 'naver' | 'kakao' | 'google' | null;
 
 const LoginPage: NextPage = () => {
   const router = useRouter();
   const [platform, setPlatform] = useState<platformType>(null);
 
   useEffect(() => {
+    const storedPlatform = localStorage.getItem('platform') as platformType;
+    storedPlatform && setPlatform(storedPlatform);
+  }, []);
+
+  useEffect(() => {
+    const code: string | null = new URL(window.location.href).searchParams.get('code');
+    if (!code || !platform) return;
+
     const handleNaverLogin = async (): Promise<void> => {
-      const code: string | null = new URL(window.location.href).searchParams.get('code');
       const success: boolean | undefined = await getNaverProcess(code);
       success && router.push('/myhome');
     };
-
     const handleGoogleLogin = async () => {
-      const code: string | null = new URL(window.location.href).searchParams.get('code');
       const success: boolean | undefined = await getGoogleProcess(code);
       success && router.push('/myhome');
     };
-
     const handleKakaoLogin = async (): Promise<void> => {
-      const code: string | null = new URL(window.location.href).searchParams.get('code');
       const success: boolean | undefined = await getKakaoProcess(code);
       success && router.push('/myhome');
     };
 
-    // handleNaverLogin();
-    handleKakaoLogin();
-    handleGoogleLogin();
-  }, []);
+    if (platform === 'naver') {
+      handleNaverLogin();
+    } else if (platform === 'kakao') {
+      handleKakaoLogin();
+    } else if (platform === 'google') {
+      handleGoogleLogin();
+    }
+  }, [platform]);
 
   return (
     <div className="flex bg-[#ffffff] w-screen h-screen">
       <div className="h-100% mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
         <MocampIcon />
-        <div className="mt-8 mb-32 text-2xl font-medium">
+        <div className="mt-[30px] mb-[135px] text-[28px] font-medium leading-[1.6]">
           <p>
             모캠프는 <span className="text-[#27CFA5] font-semibold">로그인</span> 후
           </p>
-          <p className="mt-2">시작할 수 있어요 :D</p>
+          <p>시작할 수 있어요 :D</p>
         </div>
         <div className="flex flex-col gap-5 w-[600px]">
-          <NaverLoginButton />
-          <KakaoLoginButton />
-          <GoogleLoginButton />
+          <NaverLoginButton platform={platform} />
+          <KakaoLoginButton platform={platform} />
+          <GoogleLoginButton platform={platform} />
         </div>
       </div>
     </div>
