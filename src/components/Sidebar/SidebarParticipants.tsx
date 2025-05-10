7dac7b9f017c5e6715c711acaf79c6ad643d3d2b
sumin <sumin50240585@gmail.com>
2025-05-10 17:53:21 +0900

    🎨 style : 사이드바 CSS 수정 - 가운데로 정렬렬

--- src/components/Sidebar/SidebarParticipants.tsx
+++ src/components/Sidebar/SidebarParticipants.tsx
@@ -8,7 +8,7 @@ interface SidebarParticipantsProps {
 const SidebarParticipants: React.FC<SidebarParticipantsProps> = ({ participants }) => {
     return (
     <div className="w-[200px] h-[1080px] bg-white flex items-center justify-center">
-        <div className="w-[151px] h-[64px] absolute top-[450px] rounded-[7.5px] border border-[#E8E8E8] bg-white flex flex-row items-center justify-between px-[15px] py-[10px] gap-[10px]">
+        <div className="w-[151px] h-[64px] top-[450px] rounded-[7.5px] border border-[#E8E8E8] bg-white flex flex-row items-center justify-between px-[15px] py-[10px] gap-[10px]">
             <span className="font-pre font-semibold text-[18px] leading-[1] tracking-[-0.02em] text-[#27CFA5]">
                 참여 인원
             </span>
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,6 @@
 import TodoContainer from '@/components/todo/TodoContainer';
-
+import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar'; //확인
 const mockTodos = [
   {
     id: 1,
@@ -23,16 +24,27 @@ const mockTodos = [
 
   
 ];
-
+// sidebar 목업업 //확인
+const startTime = new Date();
+const endTime = new Date(Date.now() + 60 * 60 * 1000); 
+const participants = 6;
 
 
 const WorkspacePage = () => {
   return (
+    <>
+    <Header status="workspace" />
+      <Sidebar //확인
+        startTime={startTime}
+        endTime={endTime}
+        participants={participants}
+      />
       <div className="flex items-center justify-center gap-5 bg-[#F2F2F2] w-screen h-screen">
         {mockTodos.map((todo) => (
           <TodoContainer key={todo.id} items={todo.items} />
         ))}
       </div>
+    </>
   );
 };
 
