5b4f6dfd33a5afe32250f48b248617dd89bdd202
LimJaeHyun <guswogus81@gmail.com>
2025-05-10 16:44:50 +0900

    ✨ feat: 카카오 소셜 로그인 코드 추가

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,20 +2,42 @@ import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
 import KakaoLoginButton from '@/components/auth/KakaoLoginButton';
 import NaverLoginButton from '@/components/auth/NaverLoginButton';
 import MocampIcon from '@/public/svgs/MocampIcon.svg';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import getKakaoProcess from '@/apis/getKakaoProcess';
+import type { NextPage } from 'next';
 
-const LoginPage = () => {
+const LoginPage: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // const handleNaverLogin = async (): Promise<void> => {};
+
+    // const handleKakaoLogin = async (): Promise<void> => {
+    //   const code: string | null = new URL(window.location.href).searchParams.get('code');
+    //   const success: boolean | undefined = await getKakaoProcess(code);
+    //   if (success) {
+    //     router.push('/myhome');
+    //   }
+    // };
+
+    const handleGoogleLogin = async (): Promise<void> => {};
+
+    // handleNaverLogin();
+    // handleKakaoLogin();
+    // handleGoogleLogin();
+  }, []);
+
   return (
     <div className="flex bg-[#ffffff] w-screen h-screen">
       <div className="h-100% mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
         <MocampIcon />
         <div className="mt-8 mb-32 text-2xl font-medium">
           <p>
-            모캠프는{' '}
-            <span className="text-[#27CFA5] font-semibold">로그인</span> 후
+            모캠프는 <span className="text-[#27CFA5] font-semibold">로그인</span> 후
           </p>
           <p className="mt-2">시작할 수 있어요 :D</p>
         </div>
-
         <div className="flex flex-col gap-5 w-[600px]">
           <NaverLoginButton />
           <KakaoLoginButton />
