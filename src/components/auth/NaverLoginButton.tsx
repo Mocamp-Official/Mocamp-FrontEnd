a46797f4bd89a73c07d3b5b9ef0102b715dd6451
LimJaeHyun <guswogus81@gmail.com>
2025-05-11 23:43:13 +0900

    ♻️ refactor: tailwindcss-preset-px-to-rem 라이브러리 적용을 위한 px -> rem 변환

--- src/components/auth/NaverLoginButton.tsx
+++ src/components/auth/NaverLoginButton.tsx
@@ -13,7 +13,7 @@ const NaverLoginButton: React.FC<NaverLoginButtonProps> = ({ platform }) => {
       const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login/naver/page`);
       const data = res.data;
       console.log(data);
-      // window.location.href = data;
+      window.location.href = data;
     } catch (err) {
       console.error('err');
     }
@@ -24,17 +24,17 @@ const NaverLoginButton: React.FC<NaverLoginButtonProps> = ({ platform }) => {
       onClick={() => {
         handleNaverLogin();
       }}
-      className="flex bg-[#05C050] w-full h-[100px] justify-center items-center relative border border-gray-300 rounded-[5px] group"
+      className="flex bg-[#05C050] w-full h-[6.25rem] justify-center items-center relative border border-gray-300 rounded-[0.3125rem] group"
     >
-      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 rounded-[5px] pointer-events-none transition-opacity duration-200"></div>
+      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 rounded-[0.3125rem] pointer-events-none transition-opacity duration-200"></div>
       {/* <div className="absolute left-8 w-6 h-6 bg-gray-400">로고로 변경 예정 (구글)</div> */}
-      <p className="text-[#ffffff] text-[28px] font-semibold">네이버 계정으로 시작하기</p>
+      <p className="text-[#ffffff] text-[1.75rem] font-semibold">네이버 계정으로 시작하기</p>
       <div
-        className={`absolute right-[15px] top-[-26px] items-center justify-center px-5 py-2.5 rounded-tl-[10px] rounded-tr-[10px] rounded-br-[10px] bg-white border border-[#00af83] ${
+        className={`absolute right-[0.9375rem] top-[-1.625rem] items-center justify-center px-5 py-2.5 rounded-tl-[0.625rem] rounded-tr-[0.625rem] rounded-br-[0.625rem] bg-white border border-[#00af83] ${
           platform === 'naver' ? 'flex' : 'hidden'
         }`}
       >
-        <p className="flex w-22.5 h-8 text-xl justify-center items-center font-semibold text-[#00af83] text-center">
+        <p className="flex w-[5.625rem] h-8 text-xl justify-center items-center font-semibold text-[#00af83] text-center">
           최근 로그인
         </p>
       </div>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -46,15 +46,15 @@ const LoginPage: NextPage = () => {
 
   return (
     <div className="flex bg-[#ffffff] w-screen h-screen">
-      <div className="h-100% mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
+      <div className="h-full mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
         <MocampIcon />
-        <div className="mt-[30px] mb-[135px] text-[28px] font-medium leading-[1.6]">
+        <div className="mt-[1.875rem] mb-[8.4375rem] text-[1.75rem] font-medium leading-[1.6]">
           <p>
             모캠프는 <span className="text-[#27CFA5] font-semibold">로그인</span> 후
           </p>
           <p>시작할 수 있어요 :D</p>
         </div>
-        <div className="flex flex-col gap-5 w-[600px]">
+        <div className="flex flex-col gap-5 w-[37.5rem]">
           <NaverLoginButton platform={platform} />
           <KakaoLoginButton platform={platform} />
           <GoogleLoginButton platform={platform} />
