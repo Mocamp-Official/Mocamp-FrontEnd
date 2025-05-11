47ed8d8b08808cf593c6c4ca2fce9c0932c1776d
LimJaeHyun <guswogus81@gmail.com>
2025-05-11 23:43:13 +0900

    ♻️ refactor: tailwindcss-preset-px-to-rem 라이브러리 적용을 위한 px -> rem 변환

--- src/components/auth/GoogleLoginButton.tsx
+++ src/components/auth/GoogleLoginButton.tsx
@@ -23,17 +23,17 @@ const GoogleLoginButton: React.FC<NaverLoginButtonProps> = ({ platform }) => {
       onClick={() => {
         handleGoogleLogin();
       }}
-      className="flex bg-white w-full h-[100px] justify-center items-center relative border border-gray-300 group"
+      className="flex bg-white w-full h-[6.25rem] justify-center items-center relative border border-gray-300 group"
     >
       <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 rounded pointer-events-none transition-opacity duration-200"></div>
       {/* <div className="absolute left-8 w-6 h-6 bg-gray-400">로고로 변경 예정 (구글)</div> */}
-      <p className="text-gray-700 text-[28px] font-semibold">구글 계정으로 시작하기</p>
+      <p className="text-gray-700 text-[1.75rem] font-semibold">구글 계정으로 시작하기</p>
       <div
-        className={`absolute right-[15px] top-[-26px] items-center justify-center px-5 py-2.5 rounded-tl-[10px] rounded-tr-[10px] rounded-br-[10px] bg-white border border-[#00af83] ${
+        className={`absolute right-[0.9375rem] top-[-1.625rem] items-center justify-center px-5 py-2.5 rounded-tl-[0.625rem] rounded-tr-[0.625rem] rounded-br-[0.625rem] bg-white border border-[#00af83] ${
           platform === 'google' ? 'flex' : 'hidden'
         }`}
       >
-        <p className="flex w-22.5 h-8 text-xl justify-center items-center font-semibold text-[#00af83] text-center">
+        <p className="flex w-[5.625rem] h-8 text-xl justify-center items-center font-semibold text-[#00af83] text-center">
           최근 로그인
         </p>
       </div>
