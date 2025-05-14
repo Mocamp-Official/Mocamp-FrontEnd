609883b3037e81a00903ea33d70576fa3563c2e5
LimJaeHyun <guswogus81@gmail.com>
2025-05-15 00:56:33 +0900

    ✨ feat: 마이홈 페이지 추가

--- src/pages/myhome.tsx
+++ src/pages/myhome.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import BasicHeader from '@/components/Header/BasicHeader';
+import SideBar from '@/components/myhome/SideBar';
+import Content from '@/components/myhome/Content';
 
 const myhome = () => {
-  return <div>myhome</div>;
+  return (
+    <div className="w-full h-full min-h-[1080px] bg-[#e6e6e6]">
+      <BasicHeader />
+      <div className="flex justify-center mt-[50px] gap-[10px]">
+        <SideBar />
+        <Content />
+      </div>
+    </div>
+  );
 };
 
 export default myhome;
