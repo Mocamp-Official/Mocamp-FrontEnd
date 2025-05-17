1b9587041d8d9cba5fa919c3819218d54b49b57e
chaeyoungwon <epopcy@naver.com>
2025-05-17 15:10:29 +0900

    🔧 Chore: 스타일 시스템 추가
    globals.css에 스타일 시스템 추가 및 글꼴 파일 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,17 +22,17 @@ const OnBoarding = () => {
   const slides = [<SlideCard1 />, <SlideCard1 />, <SlideCard1 />];
 
   return (
-    <div className="flex flex-col bg-white w-screen h-screen relative">
+    <div className="flex flex-col bg-gray1 w-screen h-screen relative">
       <button
         onClick={handleLogin}
-        className="absolute top-[25px] right-[320px] text-white flex items-center text-lg font-semibold tracking-[-0.02em] bg-[#27cfa5] justify-center border border-[#27cfa5] rounded-[10px] px-[30px] py-[15px]"
+        className="absolute top-[25px] h-[51px] w-[190px] shrink-0 right-[320px] text-gray1 flex items-center text-body2 !font-semibold bg-primary justify-center border border-primary rounded-[10px] px-[30px] py-[15px]"
       >
         로그인 후 시작하기
       </button>
 
       <div className="h-full mx-auto justify-center items-center flex flex-col">
         <MocampIcon className="w-[250px] h-[100px] mb-[50.05px]" />
-        <p className="text-[#4b4b4b] text-[28px] font-medium tracking-[-0.02em] mb-[49px]">
+        <p className="text-gray9 text-title2 !font-medium mb-[49px]">
           함께 모여 캠키고 성취하는&nbsp;
           <span className="text-[#27CFA5] font-semibold">우리만의 공간</span>
         </p>
@@ -40,7 +40,7 @@ const OnBoarding = () => {
         <div className="w-full max-w-[900px]">
           <Swiper
             modules={[Pagination, Autoplay]}
-            autoplay={{ delay: 5000, disableOnInteraction: false }}
+            autoplay={{ delay: 10000000, disableOnInteraction: false }}
             pagination={{ clickable: true, el: '.swiper-custom-pagination' }}
             loop
             className="w-full"
