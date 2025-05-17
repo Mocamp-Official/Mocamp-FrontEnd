bab33fb1fd0e33ed00375cd50d89f54d373451be
sumin <sumin50240585@gmail.com>
2025-05-17 15:31:50 +0900

    🎨 style : 사이드바 UI - 디자인 최종 완성

--- src/components/Sidebar/SidebarExitButton.tsx
+++ src/components/Sidebar/SidebarExitButton.tsx
@@ -1,35 +1,34 @@
-
 import React, { useState } from 'react';
 import SidebarExitModal from './modal/SidebarExitModal';
 
 const SidebarExit: React.FC = () => {
-    const [isExitModalOpen, setExitModalOpen] = useState(false);
+  const [isExitModalOpen, setExitModalOpen] = useState(false);
 
-    const handleExitClick = () => setExitModalOpen(true);
-    const handleConfirmExit = () => {
-        setExitModalOpen(false);
-    };
-    const handleCancelExit = () => setExitModalOpen(false);
+  const handleExitClick = () => setExitModalOpen(true);
+  const handleConfirmExit = () => {
+    setExitModalOpen(false);
+  };
+  const handleCancelExit = () => setExitModalOpen(false);
 
-    return (
-        <>
-            <div className="w-[150px] h-[64px] absolute left-[25px] bottom-[100px] rounded-[10px] border border-[#E8E8E8] bg-white flex items-center justify-center p-[20px]">
-                <button
-                    onClick={handleExitClick}
-                    className="w-[110px] h-[24px] flex items-center justify-center font-pre font-medium text-[18px] leading-[1] tracking-[-0.02em] text-[#555555] bg-transparent rounded"
-                >
-                    방 나가기
-                </button>
-            </div>
+  return (
+    <>
+      <div className="w-[150px] h-[64px] absolute left-[25px] bottom-[100px] rounded-[10px] border border-[#E8E8E8] bg-white flex items-center justify-center p-[20px]">
+        <button
+          onClick={handleExitClick}
+          className="w-[110px] h-[24px] flex items-center justify-center font-pre font-bold text-[18px] leading-[1] tracking-[-0.02em] text-[#555555] bg-transparent rounded"
+        >
+          방 나가기
+        </button>
+      </div>
 
-            {isExitModalOpen && (
-                <SidebarExitModal
-                    onConfirm={handleConfirmExit}
-                    onCancel={handleCancelExit}
-                />
-            )}
-        </>
-    );
+      {isExitModalOpen && (
+        <SidebarExitModal
+          onConfirm={handleConfirmExit}
+          onCancel={handleCancelExit}
+        />
+      )}
+    </>
+  );
 };
 
 export default SidebarExit;
--- src/components/Sidebar/SidebarTimer.tsx
+++ src/components/Sidebar/SidebarTimer.tsx
@@ -1,68 +1,70 @@
 import React, { useEffect, useState } from 'react';
-import { formatTime, getRemainingTime, formatRemainingTime } from '@/utils/timeUtils';
+import {
+  formatTime,
+  getRemainingTime,
+  formatRemainingTime,
+} from '@/utils/timeUtils';
 
 interface SidebarTimerProps {
-    startTime: Date;
-    endTime: Date;
+  startTime: Date;
+  endTime: Date;
 }
 
-const SidebarTimer: React.FC<SidebarTimerProps> = ({
-    startTime,
-    endTime,
-}) => {
-    const [remainingTime, setRemainingTime] = useState(getRemainingTime(endTime));
+const SidebarTimer: React.FC<SidebarTimerProps> = ({ startTime, endTime }) => {
+  const [remainingTime, setRemainingTime] = useState(getRemainingTime(endTime));
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setRemainingTime(getRemainingTime(endTime));
-        }, 1000);
-        return () => clearInterval(timer);
-    }, [endTime]);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemainingTime(getRemainingTime(endTime));
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [endTime]);
 
-    return (
-        <div className="w-[200px] h-[1080px] bg-white relative">
-            {/* 시작 / 종료 박스 */}
-            <div className="h-[126px] absolute top-[70px] left-[25px] rounded-[10px] border border-[#E8E8E8] bg-white flex flex-col gap-[19px] p-[20px]">
-                {/* 시작 */}
-                <div className="flex items-center gap-[15px]">
-                    <div className=" h-[24px] flex items-center justify-center font-pre font-semibold text-[18px] leading-[1] tracking-[-0.02em] text-[#27CFA5] bg-transparent rounded">
-                        시작
-                    </div>
-                    <div className="h-[24px] flex items-center justify-center font-pre font-medium text-[18px] leading-[1] tracking-[-0.02em] text-[#555555] bg-transparent rounded">
-                        {formatTime(startTime)}
-                    </div>
-                </div>
-
-                <div className="w-[110px] h-[1px] bg-[#E8E8E8] mx-[2px]" />
-
-                {/* 종료 */}
-                <div className="flex items-center gap-[15px]">
-                    <div className="h-[24px] flex items-center justify-center font-pre font-semibold text-[18px] leading-[1] tracking-[-0.02em] text-[#27CFA5] bg-transparent rounded">
-                        종료
-                    </div>
-                    <div className="h-[24px] flex items-center justify-center font-pre font-medium text-[18px] leading-[1] tracking-[-0.02em] text-[#555555] bg-transparent rounded">
-                        {formatTime(endTime)}
-                    </div>
-                </div>
-            </div>
-
-            {/* 남은 시간 박스 */}
-            <div className="h-[126px] absolute top-[210px] left-[25px] rounded-[10px] border border-[#E8E8E8] bg-white flex flex-col gap-[19px] p-[20px]">
-                {/* 남은 시간 */}
-                <div className="h-[24px] flex items-start font-pre font-semibold text-[18px] leading-[1] tracking-[-0.02em] text-[#27CFA5] bg-transparent rounded">
-                    남은 시간
-                </div>
-
-                {/* 구분선 */}
-                <div className="w-[110px] h-[1px] bg-[#E8E8E8] mx-[2px]" />
-
-                {/* 남은 시간 표시 */}
-                <div className="h-[24px] flex items-start font-pre font-medium text-[18px] leading-[1] tracking-[-0.02em] text-[#555555] bg-transparent rounded">
-                    {remainingTime > 0 ? formatRemainingTime(remainingTime) : '종료'}
-                </div>
-            </div>
+  return (
+    <div className="w-[200px] h-[1080px] bg-white relative">
+      {/* 시작 / 종료 박스 */}
+      <div className="w-[150px] absolute top-[70px] left-[25px] rounded-[10px] border border-[#E8E8E8] bg-white flex flex-col gap-[19px] p-[20px]">
+        {/* 시작 */}
+        <div className="flex items-center gap-[15px]">
+          <div className="h-[24px] flex items-center justify-center font-pre font-semibold text-[18px] leading-[1] tracking-[-0.02em] text-[#27CFA5]">
+            시작
+          </div>
+          <div className="h-[24px] flex items-center justify-center font-pre font-medium text-[18px] leading-[1] tracking-[-0.02em] text-[#555555]">
+            {formatTime(startTime)}
+          </div>
         </div>
-    );
-}
+
+        {/* 구분선 */}
+        <div className="w-full h-[1px] bg-[#E8E8E8]" />
+
+        {/* 종료 */}
+        <div className="flex items-center gap-[15px]">
+          <div className="h-[24px] flex items-center justify-center font-pre font-semibold text-[18px] leading-[1] tracking-[-0.02em] text-[#27CFA5]">
+            종료
+          </div>
+          <div className="h-[24px] flex items-center justify-center font-pre font-medium text-[18px] leading-[1] tracking-[-0.02em] text-[#555555]">
+            {formatTime(endTime)}
+          </div>
+        </div>
+      </div>
+
+      {/* 남은 시간 박스 */}
+      <div className="w-[150px] h-[126px] absolute top-[210px] left-[25px] rounded-[10px] border border-[#E8E8E8] bg-white flex flex-col gap-[19px] p-[20px]">
+        {/* 남은 시간 */}
+        <div className="h-[24px] flex items-start font-pre font-semibold text-[18px] leading-[1] tracking-[-0.02em] text-[#27CFA5] bg-transparent rounded">
+          남은 시간
+        </div>
+
+        {/* 구분선 */}
+        <div className="w-[110px] h-[1px] bg-[#E8E8E8] mx-[2px]" />
+
+        {/* 남은 시간 표시 */}
+        <div className="h-[24px] flex items-start font-pre font-medium text-[18px] leading-[1] tracking-[-0.02em] text-[#555555] bg-transparent rounded">
+          {remainingTime > 0 ? formatRemainingTime(remainingTime) : '종료'}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SidebarTimer;
