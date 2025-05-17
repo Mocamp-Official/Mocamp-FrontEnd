e22e838662801c05b445fe65517a08e28894248d
Congee <guswogus81@gmail.com>
2025-05-17 17:10:21 +0900

    ✨ feat: 로그인 API 연결 및 최근 로그인 기능 구현 (#26)
    * 🎨 style: printWidth 글자수 80 -> 100
    
    * ✨ feat: 카카오 로그인 기능 구현
    
    * ✨ feat: 마이홈 UI 페이지 추가
    
    * ✨ feat: 카카오 소셜 로그인 코드 추가
    
    * ♻️ refactor: 카카오 로그인 코드 수정
    
    * ✨ feat: 구글 로그인 로직 추가
    
    * ♻️ refactor: 로그인 관련 API 통합
    
    * ✨ feat: 네이버 로그인 추가
    
    * ♻️ refactor: 피그마와 맞지 않는 CSS 수정
    
    * ⬆️ update : 최근 로그인 스타일 추가 & 최근 로그인 기능 구현
    
    * ♻️ refactor: tailwindcss-preset-px-to-rem 라이브러리 적용을 위한 px -> rem 변환
    
    * ♻️ refactor: CSS 에러 수정
    
    * ♻️ refactor: 카카오 로그인 API 변경으로 인한 코드 수정
    
    * ✨ feat: axios interceptor, instance 추가
    
    * 🔧 chore: 불필요한 재렌더링 방지
    
    * ✨ feat : 플랫폼 상태 관리 추가
    
    * 🗑️ remove: 불필요한 파일 삭제
    
    * ♻️ refactor: 불필요한 코드 삭제, 파라미터 통일
    
    * ⬆️ update : 소셜 로그인 잘못된 로직 수정
    
    * 🗑️ remove: 불필요한 파일 삭제
    
    * ♻️ refactor: 선택한 플랫폼에 따라 올바른 소셜 로그인 API 호출
    
    * ♻️ refactor: 불필요한 코드 삭제
    
    * ⬆️ update : 구글 토큰 저장
    
    * ♻️ refactor: 응답 코드에 따라 일부 수정
    
    * ⬆️ update : 코드 리뷰 피드백 적용

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,22 +1,65 @@
+import { useEffect } from 'react';
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { loginByGoogle, loginByKakao, loginByNaver } from '@/apis/auth';
+import MocampIcon from '@/public/svgs/MocampIcon.svg';
 import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
 import KakaoLoginButton from '@/components/auth/KakaoLoginButton';
 import NaverLoginButton from '@/components/auth/NaverLoginButton';
-import MocampIcon from '@/public/svgs/MocampIcon.svg';
+import { platformType } from '@/types/auth';
+import { useAuthStore } from '@/stores/auth-store';
 
-const LoginPage = () => {
+const LoginPage: NextPage = () => {
+  const router = useRouter();
+  const { platform, setPlatform } = useAuthStore();
+
+  useEffect(() => {
+    const storedPlatform = localStorage.getItem('platform');
+    setPlatform(storedPlatform as platformType);
+  }, []);
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+    if (!code || !platform) return;
+
+    const redirectMap = {
+      naver: process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI,
+      kakao: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+      google: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI,
+    };
+
+    const processMap = {
+      naver: loginByNaver,
+      kakao: loginByKakao,
+      google: loginByGoogle,
+    };
+
+    const selectedRedirect = redirectMap[platform];
+    const selectedProcess = processMap[platform];
+
+    const handleLogin = async () => {
+      if (selectedRedirect && selectedProcess) {
+        const success = await selectedProcess({ code, redirectUrl: selectedRedirect });
+        if (success) {
+          router.push('/myhome');
+        }
+      }
+    };
+
+    handleLogin();
+  }, [platform]);
+
   return (
     <div className="flex bg-[#ffffff] w-screen h-screen">
-      <div className="h-100% mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
+      <div className="h-full mx-auto justify-center items-center flex flex-col bg-[#ffffff]">
         <MocampIcon />
-        <div className="mt-8 mb-32 text-2xl font-medium">
+        <div className="mt-[1.875rem] mb-[8.4375rem] text-[1.75rem] font-medium leading-[1.6]">
           <p>
-            모캠프는{' '}
-            <span className="text-[#27CFA5] font-semibold">로그인</span> 후
+            모캠프는 <span className="text-[#27CFA5] font-semibold">로그인</span> 후
           </p>
-          <p className="mt-2">시작할 수 있어요 :D</p>
+          <p>시작할 수 있어요 :D</p>
         </div>
-
-        <div className="flex flex-col gap-5 w-[600px]">
+        <div className="flex flex-col gap-5 w-[37.5rem]">
           <NaverLoginButton />
           <KakaoLoginButton />
           <GoogleLoginButton />
