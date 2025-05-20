9103a1f8dbea62b0055236e5058293bd711aad19
sumin <sumin50240585@gmail.com>
2025-05-20 13:44:56 +0900

    ✨ feat : 개인정보처리, 서비스 이용약관 페이지 - (내용 미작성)

---
+++ src/pages/personal.tsx
@@ -0,0 +1,9 @@
+const PesonalInfoPage = () => {
+  return (
+    <div className="flex bg-[#ffffff] w-screen h-screen">
+      개인정보 처리 방침
+    </div>
+  );
+};
+
+export default PesonalInfoPage;
---
+++ src/pages/service.tsx
@@ -0,0 +1,9 @@
+const ServicePage = () => {
+  return (
+    <div className="flex bg-[#ffffff] w-screen h-screen">
+      서비스 이용약관
+    </div>
+  );
+};
+
+export default ServicePage;
