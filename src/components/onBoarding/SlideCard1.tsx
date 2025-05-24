6b4df54db852674ac5cf7c7267cd7eaf7fa6dd5d
chaeyoungwon <epopcy@naver.com>
2025-05-24 15:36:18 +0900

    📱 Refactor: 온보딩 반응형 1차 구현
    온보딩 반응형 1차 구현

--- src/components/onBoarding/SlideCard1.tsx
+++ src/components/onBoarding/SlideCard1.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 const SlideCard1 = () => {
   return (
     // 전체 카드 컨테이너
-    <div className="bg-gray2 border-gray4 flex h-[500px] w-[900px] shrink-0 gap-[54px] rounded-[20px] border px-[50px]">
+    <div className="bg-gray2 border-gray4 flex h-[267px] w-[480px] shrink-0 gap-[28.67px] rounded-[20px] border px-[50px] lg:h-[375px] lg:w-[675px] lg:gap-10 xl:h-[500px] xl:w-[900px] xl:gap-[54px]">
       {/* 왼쪽 텍스트 블럭 (제목 + 설명) */}
       <div className="flex w-full flex-col gap-5">
         {/* 슬로건 제목 */}
-        <span className="text-primary text-title2 mt-[68px] flex h-[45px] items-center">
+        <span className="text-primary xl:text-title2 mt-[36.27px] flex h-[45px] items-center text-[15px] lg:mt-[51px] lg:text-[21px] xl:mt-[68px]">
           누구나 쉬운 목표 관리
         </span>
 
         {/* 설명 문구 */}
-        <span className="text-gray9 text-title3 flex h-[76px] items-center leading-[160%] font-medium">
+        <span className="text-gray9 xl:text-title3 flex h-10 items-center text-[12.8px] leading-[160%] font-medium lg:h-[58px] lg:text-lg xl:h-[76px]">
           오늘 수행할 목표를 작성하고, <br />
           체크박스를 눌러 게이지 UP!
         </span>
@@ -21,10 +21,10 @@ const SlideCard1 = () => {
       {/* 오른쪽 카드 콘텐츠 (진행도 + 투두리스트) */}
       <div className="flex flex-col justify-center gap-[18px]">
         {/* 진행도 박스 */}
-        <div className="bg-gray1 border-gray4 mt-[3px] flex flex-col gap-5 rounded-[20px] border p-[30px]">
-          <div className="flex items-center gap-[20.38px]">
+        <div className="bg-gray1 border-gray4 mt-[3px] flex flex-col gap-5 rounded-[10.67px] border p-4 lg:rounded-[15px] lg:p-[22.5px] xl:rounded-[20px] xl:p-[30px]">
+          <div className="flex items-center gap-[10.37px] lg:gap-[15.06px] xl:gap-[20.38px]">
             {/* 진행 상태 뱃지 */}
-            <span className="text-blue border-blue rounded-4xl border bg-[#D7F0FF] px-[30px] py-[10px] text-lg font-medium">
+            <span className="text-blue border-blue rounded-4xl border bg-[#D7F0FF] px-4 py-[5.33px] text-[9.6px] font-medium lg:px-[22.5px] lg:py-[7.5px] lg:text-[13.5px] xl:px-[30px] xl:py-[10px] xl:text-lg">
               1/2
             </span>
 
@@ -33,13 +33,13 @@ const SlideCard1 = () => {
           </div>
 
           {/* 게이지 바 */}
-          <div className="bg-gray3 relative h-[27.84px] w-[417.7px] rounded-[20px]">
-            <div className="bg-primary absolute top-0 left-0 h-full w-[208.5px] rounded-[20px]" />
+          <div className="bg-gray3 relative h-[14.85px] w-[222.75px] rounded-[10.67px] lg:h-[20.88px] lg:w-[313.25px] lg:rounded-[15px] xl:h-[27.84px] xl:w-[417.7px] xl:rounded-[20px]">
+            <div className="bg-primary absolute top-0 left-0 h-full w-[111px] rounded-[10.67px] lg:w-[156.37px] lg:rounded-[15px] xl:w-[208.5px] xl:rounded-[20px]" />
           </div>
         </div>
 
         {/* 투두 리스트 박스 */}
-        <div className="border-gray4 flex flex-col gap-5 rounded-[20px] border bg-white p-[50px]">
+        <div className="border-gray4 flex flex-col gap-5 rounded-[10.67px] border bg-white p-[50px] lg:rounded-[15px] xl:rounded-[20px]">
           {/* 투두 항목 1 (완료) */}
           <div className="flex items-center gap-5">
             <div className="border-primary h-10 w-10 rounded-[10px] border bg-[#BEF1E4]" />
