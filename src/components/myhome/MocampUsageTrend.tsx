f5e4c2e173137f97ce1bb20fe912fed1b72d260f
LimJaeHyun <guswogus81@gmail.com>
2025-05-24 15:56:58 +0900

    ✨ feat: 프로필 설정, 참여한 모캠프, 모캠프 사용 추이 컴포넌트 추가

---
+++ src/components/myhome/MocampUsageTrend.tsx
@@ -0,0 +1,9 @@
+const MocampUsageTrend = () => {
+  return (
+    <div className="w-[982px] h-[880px] rounded-[20px] bg-[#ffffff] p-8 flex flex-col">
+      MocampUsageTrend
+    </div>
+  );
+};
+
+export default MocampUsageTrend;
