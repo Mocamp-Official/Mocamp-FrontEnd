3d51ebaf7b27a9f75ac7c2bd85a918c2d60a342a
LimJaeHyun <guswogus81@gmail.com>
2025-05-24 15:56:27 +0900

    ♻️ refactor: 사이드 바 동적 클릭 CSS 추가

--- src/components/myhome/SideBar.tsx
+++ src/components/myhome/SideBar.tsx
@@ -1,6 +1,9 @@
+import { useCategoryStore } from '@/stores/category-store';
 import Image from 'next/image';
 
 const SideBar = () => {
+  const { category, setCategory } = useCategoryStore();
+
   return (
     <div className="w-[18rem] h-[55rem] rounded-[1.25rem] bg-white flex flex-col items-center relative overflow-hidden">
       {/* 사이드바 헤더 */}
@@ -8,7 +11,6 @@ const SideBar = () => {
         <p className="text-xl font-semibold text-white">마이페이지</p>
       </div>
 
-      {/* 유저 프로필 이미지 */}
       <div className="mt-[3.125rem] relative w-[12.5rem] h-[12.3125rem] flex items-center justify-center rounded-full border-2 border-[#27CFA5]">
         <Image
           src="/defaultProfile.png"
@@ -18,21 +20,55 @@ const SideBar = () => {
           className="rounded-full"
         />
       </div>
+      <p className="mt-[1.875rem] text-[1.75rem] font-semibold text-[#555555]">이주은님</p>
 
-      {/* 유저 이름 */}
-      <p className="mt-[1.875rem] text-[1.75rem] font-semibold text-[#555]">이주은님</p>
-
-      {/* 메뉴 박스 */}
-      <div className="mt-[1.875rem] w-[14.25rem] h-[11.75rem] flex flex-col gap-4 p-[1.25rem] bg-white border border-[#e8e8e8] rounded-[0.625rem]">
-        <p className="text-xl font-semibold text-[#27cfa5]">프로필 설정</p>
+      <div className="mt-[1.875rem] w-[14.25rem] h-[250px] flex flex-col gap-4 p-[1.25rem] bg-white border border-[#e8e8e8] rounded-[0.625rem]">
+        <p
+          onClick={() => {
+            setCategory('MYHOME_TOTAL');
+          }}
+          className={`text-xl font-medium cursor-pointer ${
+            category === 'MYHOME_TOTAL' ? 'text-[#27cfa5]' : 'text-[#555555]'
+          }`}
+        >
+          마이 홈 전체보기
+        </p>
         <hr className="border-[#e8e8e8]" />
-        <p className="text-xl font-medium text-[#555555]">참여한 모캠프</p>
+        <p
+          onClick={() => {
+            setCategory('PROFILE_SETTING');
+          }}
+          className={`text-xl font-semibold cursor-pointer ${
+            category === 'PROFILE_SETTING' ? 'text-[#27cfa5]' : 'text-[#555555]'
+          }`}
+        >
+          프로필 설정
+        </p>
         <hr className="border-[#e8e8e8]" />
-        <p className="text-xl font-medium text-[#555555]">모캠프 사용 추이</p>
+        <p
+          onClick={() => {
+            setCategory('PARTICIPANTED_MOCAMP');
+          }}
+          className={`text-xl font-medium cursor-pointer ${
+            category === 'PARTICIPANTED_MOCAMP' ? 'text-[#27cfa5]' : 'text-[#555555]'
+          }`}
+        >
+          참여한 모캠프
+        </p>
+        <hr className="border-[#e8e8e8]" />
+        <p
+          onClick={() => {
+            setCategory('MOCAMP_USAGE_TREND');
+          }}
+          className={`text-xl font-medium cursor-pointer ${
+            category === 'MOCAMP_USAGE_TREND' ? 'text-[#27cfa5]' : 'text-[#555555]'
+          }`}
+        >
+          모캠프 사용 추이
+        </p>
       </div>
 
-      {/* 로그아웃 버튼 */}
-      <button className="mt-[10.8125rem] w-[14.25rem] h-[4.375rem] bg-white border border-[#e8e8e8] rounded-[0.625rem] text-xl font-semibold text-[#555555]">
+      <button className="mt-[121px] w-[14.25rem] h-[4.375rem] bg-white border border-[#e8e8e8] rounded-[0.625rem] text-xl font-semibold text-[#555555]">
         로그아웃
       </button>
     </div>
