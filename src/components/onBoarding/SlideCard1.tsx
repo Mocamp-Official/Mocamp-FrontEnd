8c767bcb49c431e3c2d51177ba522cea9796aa0b
chaeyoungwon <epopcy@naver.com>
2025-06-02 01:26:32 +0900

    📱 Feat: 온보딩 Slide card 3 반응형 추가
    온보딩 반응형 최종 구현

--- src/components/onBoarding/SlideCard1.tsx
+++ src/components/onBoarding/SlideCard1.tsx
@@ -35,9 +35,9 @@ const SlideCard1 = () => {
                 마이크 설정 <span className="text-primary">*</span>
               </span>
             </div>
-            <div className="bg-gray3 text-gray9 h-fit max-h-[69.84px] min-w-[78.6px] rounded-[4.14px] px-[25.04px] py-[9.49px] text-[15.51px] font-medium lg:rounded-[5.82px] lg:px-[35.15px] lg:py-[13.37px] xl:rounded-[7.76px] xl:px-[46.7px] xl:py-[17.66px]">
-              <div className="bg-primary w-fit rounded-[413.44px] px-[8.28px] py-[4.14px] text-center text-[8.28px] font-semibold text-white lg:px-[11.64px] lg:py-[5.82px] lg:text-[11.64px] xl:px-[15.52px] xl:py-[7.76px] xl:text-[15.52px]">
-                ON
+            <div className="bg-gray3 text-gray9 h-fit max-h-[69.84px] min-w-[78.6px] rounded-[4.14px] px-[25.04px] py-[9.49px] text-[15.51px] font-medium lg:rounded-[5.82px] lg:px-[35.15px] lg:py-[13.37px] xl:min-w-[146.7px] xl:rounded-[7.76px] xl:px-[46.7px] xl:py-[17.66px]">
+              <div className="bg-primary flex max-h-[34.5px] w-fit items-center justify-center rounded-[413.44px] px-[8.28px] py-[4.14px] text-center text-[8.28px] font-semibold text-white lg:px-[11.64px] lg:py-[5.82px] lg:text-[11.64px] xl:px-[15.52px] xl:py-[7.76px] xl:text-[15.52px]">
+                <span>ON</span>
               </div>
             </div>
           </div>
@@ -52,7 +52,7 @@ const SlideCard1 = () => {
                 최대 12시간
               </span>
             </div>
-            <div className="bg-gray3 text-gray9 flex h-fit min-h-[37.25px] min-w-[144.85px] items-center justify-center gap-[4.14px] rounded-[4.14px] px-[27.66px] py-[10.35px] text-[9.9325px] font-medium lg:min-h-[52.37px] lg:min-w-[203.7px] lg:gap-[5.82px] lg:rounded-[5.8198px] lg:px-[39.34px] lg:py-[14.55px] lg:text-[13.967px] xl:min-h-[69.84px] xl:min-w-[271.59px] xl:gap-[7.76px] xl:rounded-[7.76px] xl:px-[52.62px] xl:py-[19.4px] xl:text-[18.623px]">
+            <div className="bg-gray3 text-gray9 flex h-fit max-h-[37.25px] min-w-[144.85px] items-center justify-center gap-[4.14px] rounded-[4.14px] px-[27.66px] py-[10.35px] text-[9.9325px] font-medium lg:min-h-[52.37px] lg:min-w-[203.7px] lg:gap-[5.82px] lg:rounded-[5.8198px] lg:px-[39.34px] lg:py-[14.55px] lg:text-[13.967px] xl:min-h-[69.84px] xl:min-w-[271.59px] xl:gap-[7.76px] xl:rounded-[7.76px] xl:px-[52.62px] xl:py-[19.4px] xl:text-[18.623px]">
               <div className="text-primary border-gray4 w-fit rounded-[4.1385px] border-[0.4139px] bg-white px-[8.28px] py-[2.07px] text-center lg:px-[11.64px] lg:py-[2.91px] xl:px-[15.52px] xl:py-[3.88px]">
                 3
               </div>
@@ -73,8 +73,8 @@ const SlideCard1 = () => {
                 최대 5명
               </span>
             </div>
-            <div className="bg-gray3 text-gray9 flex h-fit max-h-[69.84px] min-w-[78.6px] items-center gap-[4.14px] rounded-[4.14px] px-[25.04px] py-[9.49px] text-[9.9325px] font-medium lg:gap-[5.82px] lg:rounded-[5.82px] lg:px-[35.15px] lg:py-[13.37px] lg:text-[13.967px] xl:gap-[7.76px] xl:rounded-[7.76px] xl:px-[46.7px] xl:py-[17.66px] xl:text-[18.623px]">
-              <div className="text-primary border-gray4 w-fit rounded-[4.1385px] border-[0.4139px] bg-white px-[8.28px] py-[2.07px] text-center lg:px-[11.64px] lg:py-[2.91px] xl:px-[15.52px] xl:py-[3.88px]">
+            <div className="bg-gray3 text-gray9 flex h-fit max-h-[69.84px] min-w-[78.6px] items-center gap-[4.14px] rounded-[4.14px] px-[20.97px] py-[10.35px] text-[9.9325px] font-medium lg:gap-[5.82px] lg:rounded-[5.82px] lg:px-[29.74px] lg:py-[14.55px] lg:text-[13.967px] xl:min-w-[146.7px] xl:gap-[7.76px] xl:rounded-[7.76px] xl:px-[39.82px] xl:py-[19.4px] xl:text-[18.623px]">
+              <div className="text-primary border-gray4 w-fit gap-[4.14px] rounded-[4.1385px] border-[0.4139px] bg-white px-[8.28px] py-[2.07px] text-center lg:px-[11.64px] lg:py-[2.91px] xl:px-[15.52px] xl:py-[3.88px]">
                 4
               </div>
               <span>명</span>
--- src/components/onBoarding/SlideCard2.tsx
+++ src/components/onBoarding/SlideCard2.tsx
@@ -1,57 +1,32 @@
 import React from 'react';
 import SlideCardLayout from './SlideCardLayout';
+import Graph from '@/public/svgs/OnBoardingGraph.svg';
+import Todo from '@/public/svgs/OnBoardingTodo.svg';
 
 const SlideCard2 = () => {
   return (
     <SlideCardLayout
-      title="누구나 쉬운 목표 관리"
+      title="한 눈에 보는 성취도"
       description={
         <>
-          오늘 수행할 목표를 작성하고,
+          작업한 시간과 달성한 목표를
           <br />
-          체크박스를 눌러 게이지 UP!
+          확인하고 성취를 느껴보세요!
         </>
       }
     >
-      {/* 오른쪽 카드 콘텐츠 (진행도 + 투두리스트) */}
-      <div className="flex h-fit min-w-64 flex-col justify-center gap-[9.8px] lg:gap-[13.25px] xl:gap-[18px]">
-        {/* 진행도 박스 */}
-        <div className="bg-gray1 border-gray4 flex flex-col gap-[10.67px] rounded-[10.67px] border p-4 lg:gap-[15px] lg:rounded-[15px] lg:p-[22.5px] xl:gap-5 xl:rounded-[20px] xl:p-[30px]">
-          <div className="flex items-center gap-[10.37px] lg:gap-[15.06px] xl:gap-[20.38px]">
-            {/* 진행 상태 뱃지 */}
-            <span className="text-blue border-blue rounded-4xl border bg-[#D7F0FF] px-4 py-[5.33px] text-[9.6px] font-medium lg:px-[22.5px] lg:py-[7.5px] lg:text-[13.5px] xl:px-[30px] xl:py-[10px] xl:text-lg">
-              1/2
-            </span>
-
-            {/* 진행 설명 */}
-            <span className="text-gray9 xl:text-body1 text-[10.67px] font-medium lg:text-[15px]">
-              목표 두 개 꼭 완수하기!
-            </span>
+      <div className="flex h-fit min-w-64 flex-col justify-center gap-[9.8px] lg:gap-[7.5px] xl:gap-[18px]">
+        <div className="bg-gray1 border-gray4 flex flex-col rounded-[10.67px] border pt-[8.53px] pr-[16.14px] pb-2 pl-4 lg:w-90 lg:rounded-[15px] lg:p-[22.5px] lg:text-[5.71px] xl:w-120 xl:rounded-[20px] xl:px-[30px] xl:pt-4 xl:pb-[15px]">
+          <div className="bg-primary flex h-[12.7px] items-center justify-center rounded-t-[2.5318px] text-[4.06px] font-semibold text-white lg:h-[17.85px] lg:rounded-t-[3.5692px] xl:h-[23.8px] xl:w-105 xl:rounded-t-[4.7589px] xl:text-[7.6142px]">
+            주은님이 그동안 달성한 목표 수는?
           </div>
-
-          {/* 게이지 바 */}
-          <div className="bg-gray3 relative h-[14.85px] w-[222.75px] rounded-[10.67px] lg:h-[20.88px] lg:w-[313.25px] lg:rounded-[15px] xl:h-[27.84px] xl:w-[417.7px] xl:rounded-[20px]">
-            <div className="bg-primary absolute top-0 left-0 h-full w-[111px] rounded-[10.67px] lg:w-[156.37px] lg:rounded-[15px] xl:w-[208.5px] xl:rounded-[20px]" />
+          <div className="border-gray4 text-gray9 mb-[0.38px] ml-[0.18px] flex h-[29.67px] items-center justify-center rounded-b-[2.5318px] border-[0.2358px] border-t-0 text-[7.106px] font-semibold tracking-[-0.02em] lg:h-[41.75px] lg:rounded-b-[3.5692px] lg:text-[9.99px] xl:h-[55.7px] xl:w-105 xl:rounded-b-[4.7589px] xl:text-[13.3249px]">
+            그동안 주은님은 123개의 목표를 달성했어요!
           </div>
         </div>
-
-        {/* 투두 리스트 박스 */}
-        <div className="border-gray4 flex flex-col gap-[10.67px] rounded-[10.67px] border bg-white p-[26.67px] lg:gap-[15px] lg:rounded-[15px] lg:p-[37.5px] xl:gap-5 xl:rounded-[20px] xl:p-[50px]">
-          {/* 투두 항목 1 (완료) */}
-          <div className="l flex items-center gap-[10.67px] lg:gap-[15px] xl:gap-5">
-            <div className="border-primary h-[21.33px] w-[21.33px] rounded-[5.3px] border bg-[#BEF1E4] lg:h-[30px] lg:w-[30px] lg:rounded-[7.5px] xl:h-10 xl:w-10 xl:rounded-[10px]" />
-            <span className="text-gray6 xl:text-body1 text-[10.67px] font-medium line-through lg:text-[15px]">
-              디자인 가이드라인 설정하기
-            </span>
-          </div>
-
-          {/* 투두 항목 2 (미완료) */}
-          <div className="flex items-center gap-[10.67px] lg:gap-[15px] xl:gap-5">
-            <div className="border-gray4 bg-gray1 h-[21.33px] w-[21.33px] rounded-[5.3px] border lg:h-[30px] lg:w-[30px] lg:rounded-[7.5px] xl:h-10 xl:w-10 xl:rounded-[10px]" />
-            <span className="xl:text-body1 text-gray9 text-[10.67px] font-medium lg:text-[15px]">
-              중충실도 와이어프레임 설계하기
-            </span>
-          </div>
+        <div className="bg-gray1 border-gray4 flex h-32 items-end gap-[7.28px] rounded-[10.67px] border px-[28.27px] pt-[6.93px] pb-[7.5px] lg:h-45 lg:w-90 lg:gap-[10.24px] lg:rounded-[15px] lg:px-[39.75px] lg:pt-[9.75px] lg:pb-[10.55px] xl:h-60 xl:w-120 xl:gap-[13.65px] xl:rounded-[20px] xl:p-[30px] xl:px-[53px] xl:pt-[13px] xl:pb-[14.07px]">
+          <Graph className="h-[95px] lg:h-[133px] lg:w-[186px] xl:h-[178px] xl:w-[248px]" />
+          <Todo className="h-[114px] lg:h-40 lg:w-[84.6px] xl:h-[213px] xl:w-[113px]" />
         </div>
       </div>
     </SlideCardLayout>
