408adc8c488a5e7e2e74b2dd3a357f90757208d5
sumin <sumin50240585@gmail.com>
2025-06-03 13:00:57 +0900

    ✨ feat : 푸터 조항 페이지 추가

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,9 @@
+import Link from 'next/link';
 import { INQUIRY_FORM_URL, INSTAGRAM_URL } from '@/constants/link';
-import { useRouter } from 'next/router';
+import { PERSONAL_POLICY_PATH, SERVICE_POLICY_PATH } from '@/constants/routes';
 import MocampWhite from '@/public/svgs/MocampIconWhite.svg';
 
 const Footer = () => {
-  const router = useRouter();
-
-  const handlePesonalInfoClick = () => {
-    router.push('/personal');
-  };
-  const handleServiceClick = () => {
-    router.push('/service');
-  };
-
   return (
     <footer className="w-full bg-[#27CFA5] py-12">
       <div className="max-w-[1200px] mx-auto flex justify-between">
@@ -22,21 +14,19 @@ const Footer = () => {
             style={{ aspectRatio: '150/60.54' }}
           />
 
-          {/* 정책  */}
+          {/* 정책 */}
           <div>
             <div className="flex items-center gap-6">
-              <a
-                onClick={handlePesonalInfoClick}
-                className="text-body2 text-white/70"
-              >
-                개인정보처리방침
-              </a>
-              <a
-                onClick={handleServiceClick}
-                className="text-body2 text-white/70"
-              >
-                서비스 이용약관
-              </a>
+              <Link href={PERSONAL_POLICY_PATH} passHref>
+                <span className="text-body2 text-white/70 cursor-pointer">
+                  개인정보처리방침
+                </span>
+              </Link>
+              <Link href={SERVICE_POLICY_PATH} passHref>
+                <span className="text-body2 text-white/70 cursor-pointer">
+                  서비스 이용약관
+                </span>
+              </Link>
             </div>
             <div className="text-body2 text-white mt-2">
               © 2025 Mocamp Inc. All rights reserved.
@@ -47,18 +37,22 @@ const Footer = () => {
         {/* 버튼 */}
         <div className="flex flex-col justify-end">
           <div className="flex gap-4 mt-2">
-            <button
+            <a
+              href={INQUIRY_FORM_URL}
+              target="_blank"
+              rel="noopener noreferrer" 
               className="inline-flex items-center justify-center px-5 py-2.5 rounded-[5px] border border-white/50 bg-[#27CFA5] text-body2 font-bold text-white"
-              onClick={() => window.open(INQUIRY_FORM_URL, '_blank')}
             >
               문의사항
-            </button>
-            <button
+            </a>
+            <a
+              href={INSTAGRAM_URL}
+              target="_blank"
+              rel="noopener noreferrer" 
               className="inline-flex items-center justify-center px-5 py-2.5 rounded-[5px] border border-white/50 bg-[#27CFA5] text-body2 font-bold text-white"
-              onClick={() => window.open(INSTAGRAM_URL, '_blank')}
             >
               인스타그램
-            </button>
+            </a>
           </div>
         </div>
       </div>
