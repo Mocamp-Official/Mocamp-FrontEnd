9fb1ef5a0899ecc891e046456300418296198f3f
chaeyoungwon <epopcy@naver.com>
2025-06-28 00:15:24 +0900

    ✨ Feat: 온보딩 페이지 레이아웃 컴포넌트  추가
    feat: 온보딩 페이지 레이아웃 컴포넌트 추가

--- src/components/Header/OnboardingHeader.tsx
+++ src/components/Header/OnboardingHeader.tsx
@@ -2,37 +2,25 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 const OnboardingHeader = () => {
-const router = useRouter();
+  const router = useRouter();
 
-const handleLoginClick = () => {
-    router.push('/login'); 
-};
-    return (
-    <header className="w-full bg-white h-[100px] min-w-[1920px]">
-    <div className="relative w-[1920px] h-full mx-auto">
-    {/* 버튼(기존 디자인에서 패딩값, 폰트 크기 차이 존재*/}
-    <button
-        type="button"
-        onClick={handleLoginClick}
-        className="
-            absolute top-[25px] left-[1410px]
-            w-[190px] h-[51px]
-            rounded-[10px] border border-solid border-[#27CFA5]
-            px-[20px] py-[10px]
-            font-pre font-semibold
-            text-[17px] leading-[100%] tracking-[-0.02em]
-            text-white
-            bg-[#27CFA5]
-            hover:bg-teal-500
-            flex items-center justify-center"
+  const handleLoginClick = () => {
+    router.push('/login');
+  };
+  return (
+    <header className="h-[100px] w-full min-w-[1024px] bg-white">
+      <div className="relative mx-auto h-full w-full min-w-[1024px]">
+        {/* 버튼(기존 디자인에서 패딩값, 폰트 크기 차이 존재*/}
+        <button
+          type="button"
+          onClick={handleLoginClick}
+          className="font-pre absolute top-[25px] right-[170px] flex h-[51px] w-[190px] cursor-pointer items-center justify-center rounded-[10px] border border-solid border-[#27CFA5] bg-[#27CFA5] px-[20px] py-[10px] text-[17px] leading-[100%] font-semibold tracking-[-0.02em] text-white lg:right-[239.5px] xl:right-80"
         >
-        로그인 후 시작하기
+          로그인 후 시작하기
         </button>
-    </div>
+      </div>
     </header>
-);
+  );
 };
 
 export default OnboardingHeader;
-
-
