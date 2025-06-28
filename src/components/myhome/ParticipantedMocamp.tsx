29bd4b4e310469c638647f7302819546da4033a3
LimJaeHyun <guswogus81@gmail.com>
2025-06-28 11:39:46 +0900

    ✨ feat: 참여한 모캠프 UI 구현

--- src/components/myhome/ParticipantedMocamp.tsx
+++ src/components/myhome/ParticipantedMocamp.tsx
@@ -1,8 +1,32 @@
-// ParticipantedMocamp.tsx
+import ParticipantedMocampItem from './content/ParticipantedMocampItem';
+
 const ParticipantedMocamp = () => {
   return (
-    <div className="flex h-[880px] w-[982px] flex-col rounded-[20px] bg-[#ffffff] p-8 text-2xl">
-      참여한 모캠프
+    <div className="flex h-[880px] w-[982px] flex-col rounded-[20px] bg-[#ffffff] p-12.5 text-2xl">
+      <p className="mb-7.5">최근 참여한 모캠프</p>
+      <div className="flex flex-col gap-5 overflow-y-auto">
+        <ParticipantedMocampItem
+          size="lg"
+          isCompleted={false}
+          roomName="은학샘과 아이들"
+          createdAt="2025. 04. 27"
+          time="3h 30m"
+        />
+        <ParticipantedMocampItem
+          size="lg"
+          isCompleted={false}
+          roomName="은학샘과 아이들"
+          createdAt="2025. 04. 27"
+          time="3h 30m"
+        />
+        <ParticipantedMocampItem
+          size="lg"
+          isCompleted={true}
+          roomName="은학샘과 아이들"
+          createdAt="2025. 04. 27"
+          time="3h 30m"
+        />
+      </div>
     </div>
   );
 };
