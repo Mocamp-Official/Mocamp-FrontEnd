769521bf72fa07c6a1c5d6164f9c3bb7a17851d5
LimJaeHyun <guswogus81@gmail.com>
2025-06-28 20:53:56 +0900

    ♻️ refactor: 마이홈 헤더 수정

--- src/components/Header/CreateJoinHeader.tsx
+++ src/components/Header/CreateJoinHeader.tsx
@@ -1,15 +1,15 @@
+import { MYHOME_PATH, ROOM_CREATE_PATH } from '@/constants/routes';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 const CreateJoinHeader = () => {
+  const router = useRouter();
   const [isCreateModalOpen, setCreateModalOpen] = useState(false);
-  const [isJoinModalOpen, setJoinModalOpen] = useState(false);
 
   const handleCreateClick = () => setCreateModalOpen(true);
-  const handleJoinClick = () => setJoinModalOpen(true);
 
   const closeModals = () => {
     setCreateModalOpen(false);
-    setJoinModalOpen(false);
   };
 
   return (
@@ -18,19 +18,25 @@ const CreateJoinHeader = () => {
         <img
           src="/svgs/MocampIcon.svg"
           alt="모캠프 로고"
-          className="my-auto h-[48.46px] w-[120px]"
+          className="my-auto h-[48.46px] w-[120px] cursor-pointer"
+          onClick={() => {
+            router.asPath === MYHOME_PATH ? router.reload() : router.push(MYHOME_PATH);
+          }}
         />
         <div className="my-auto flex flex-row gap-[8.45px]">
           <button
             type="button"
-            className="my-auto flex h-[50.4px] w-[171px] items-center justify-center rounded-[10px] bg-gray-100 text-[17.9px] leading-[100%] font-semibold tracking-[-0.02em] text-[#555555] hover:bg-gray-200"
+            className="my-auto flex h-[50.4px] w-[171px] cursor-pointer items-center justify-center rounded-[10px] bg-gray-100 text-[17.9px] leading-[100%] font-semibold tracking-[-0.02em] text-[#555555] hover:bg-gray-200"
           >
             모캠프 참여하기
           </button>
 
           <button
             type="button"
-            className="my-auto flex h-[50.4px] w-[171px] items-center justify-center rounded-[10px] bg-[#27CFA5] text-[17.9px] leading-[100%] font-semibold tracking-[-0.02em] text-white hover:bg-teal-500"
+            className="my-auto flex h-[50.4px] w-[171px] cursor-pointer items-center justify-center rounded-[10px] bg-[#27CFA5] text-[17.9px] leading-[100%] font-semibold tracking-[-0.02em] text-white hover:bg-teal-500"
+            onClick={() => {
+              router.push(ROOM_CREATE_PATH);
+            }}
           >
             모캠프 생성하기
           </button>
