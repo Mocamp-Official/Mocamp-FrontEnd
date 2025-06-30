a8f32154671ecffa99ea5166606d7de3a4b0ddec
chaeyoungwon <epopcy@naver.com>
2025-06-30 13:29:22 +0900

    📱 Design: 푸터 반응형 및 온보딩 반응형 추가
    푸터 반응형 및 온보딩 반응형 추가

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,30 +5,26 @@ import MocampWhite from '@/public/svgs/MocampIconWhite.svg';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-[#27CFA5] py-12">
-      <div className="max-w-[1200px] mx-auto flex justify-between">
+    <footer className="bg-primary w-screen py-[26.87px] lg:py-[37.25px] xl:py-12">
+      <div className="mx-auto flex w-full justify-center gap-[153.67px] lg:gap-[215.75px] xl:gap-72">
         <div>
           {/* 로고 */}
           <MocampWhite
-            className="w-[150px] h-[60.54px] mb-4 flex-shrink-0"
+            className="mb-4 h-[32.287px] w-20 flex-shrink-0 lg:mb-7 lg:h-[45.404px] lg:w-[112.5px] xl:mb-[37.46px] xl:h-[60.54px] xl:w-[150px]"
             style={{ aspectRatio: '150/60.54' }}
           />
 
           {/* 정책 */}
           <div>
-            <div className="flex items-center gap-6">
+            <div className="xl:text-body2 flex items-center gap-[9.73px] text-[9.6px] lg:gap-[14.5px] lg:text-[13.5px] xl:gap-5">
               <Link href={PERSONAL_POLICY_PATH} passHref>
-                <span className="text-body2 text-white/70 cursor-pointer">
-                  개인정보처리방침
-                </span>
+                <span className="cursor-pointer text-white/70">개인정보처리방침</span>
               </Link>
               <Link href={SERVICE_POLICY_PATH} passHref>
-                <span className="text-body2 text-white/70 cursor-pointer">
-                  서비스 이용약관
-                </span>
+                <span className="cursor-pointer text-white/70">서비스 이용약관</span>
               </Link>
             </div>
-            <div className="text-body2 text-white mt-2">
+            <div className="mt-[5.53px] text-white lg:mt-[7.25px] xl:mt-2.5">
               © 2025 Mocamp Inc. All rights reserved.
             </div>
           </div>
@@ -36,20 +32,20 @@ const Footer = () => {
 
         {/* 버튼 */}
         <div className="flex flex-col justify-end">
-          <div className="flex gap-4 mt-2">
+          <div className="xl:text-body2 mt-2 flex gap-4 text-[9.6px] lg:text-[13.5px]">
             <a
               href={INQUIRY_FORM_URL}
               target="_blank"
-              rel="noopener noreferrer" 
-              className="inline-flex items-center justify-center px-5 py-2.5 rounded-[5px] border border-white/50 bg-[#27CFA5] text-body2 font-bold text-white"
+              rel="noopener noreferrer"
+              className="inline-flex items-center justify-center rounded-[5px] border border-white/50 bg-[#27CFA5] px-[10.67px] py-[5.33px] font-bold text-white lg:px-[15px] lg:py-[7.5px] xl:px-5 xl:py-2.5"
             >
               문의사항
             </a>
             <a
               href={INSTAGRAM_URL}
               target="_blank"
-              rel="noopener noreferrer" 
-              className="inline-flex items-center justify-center px-5 py-2.5 rounded-[5px] border border-white/50 bg-[#27CFA5] text-body2 font-bold text-white"
+              rel="noopener noreferrer"
+              className="inline-flex items-center justify-center rounded-[5px] border border-white/50 bg-[#27CFA5] px-[10.67px] py-[5.33px] font-bold text-white lg:px-[15px] lg:py-[7.5px] xl:px-5 xl:py-2.5"
             >
               인스타그램
             </a>
--- src/components/Header/OnboardingHeader.tsx
+++ src/components/Header/OnboardingHeader.tsx
@@ -8,13 +8,12 @@ const OnboardingHeader = () => {
     router.push('/login');
   };
   return (
-    <header className="h-[100px] w-full min-w-[1024px] bg-white">
-      <div className="relative mx-auto h-full w-full min-w-[1024px]">
-        {/* 버튼(기존 디자인에서 패딩값, 폰트 크기 차이 존재*/}
+    <header className="h-[53.5px] w-screen bg-white lg:h-19 xl:h-[100px]">
+      <div className="relative mx-auto h-full w-screen">
         <button
           type="button"
           onClick={handleLoginClick}
-          className="font-pre absolute top-[25px] right-[170px] flex h-[51px] w-[190px] cursor-pointer items-center justify-center rounded-[10px] border border-solid border-[#27CFA5] bg-[#27CFA5] px-[20px] py-[10px] text-[17px] leading-[100%] font-semibold tracking-[-0.02em] text-white lg:right-[239.5px] xl:right-80"
+          className="font-pre absolute top-[13.3px] right-[170px] flex h-[27px] w-fit cursor-pointer items-center justify-center rounded-[5.333px] border border-solid border-[#27CFA5] bg-[#27CFA5] px-4 py-2 text-[9.6px] leading-[100%] font-semibold tracking-[-0.02em] text-white lg:top-[18.75px] lg:right-[239.5px] lg:h-[38.5px] lg:rounded-[7.5px] lg:px-[22.5px] lg:py-[11.25px] lg:text-[13.5px] xl:top-[25px] xl:right-80 xl:h-[51px] xl:w-[190px] xl:rounded-[10px] xl:px-[20px] xl:py-[10px] xl:text-[17px]"
         >
           로그인 후 시작하기
         </button>
