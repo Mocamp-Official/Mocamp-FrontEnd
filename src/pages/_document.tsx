332cffceb0adc91046b2365c70fb404364724189
sumin <sumin50240585@gmail.com>
2025-07-03 02:01:23 +0900

    ✨ feat :  카카오 SDK 로드

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,9 @@ export default function Document() {
         {/* 카카오 SDK 스크립트 */}
         <script
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.5/kakao.min.js"
-    integrity="sha384-dok87au0gKqJdxs7msEdBPNnKSRT+/mhTVzq+qOhcL464zXwvcrpjeWvyj1kCdq6"
-    crossOrigin="anonymous"
+          integrity="sha384-dok87au0gKqJdxs7msEdBPNnKSRT+/mhTVzq+qOhcL464zXwvcrpjeWvyj1kCdq6"
+          crossOrigin="anonymous"
+          defer
         />
       </Head>
       <body>
@@ -18,4 +19,3 @@ export default function Document() {
     </Html>
   );
 }
-
