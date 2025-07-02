c8bac222690c4410d2322a3da6e0135bf18b0492
sumin <sumin50240585@gmail.com>
2025-07-03 03:07:55 +0900

    ✨ feat :  카카오 SDK 로드 - app.tsx 병합

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+// pages/_app.tsx
 import localFont from 'next/font/local';
+import Script from 'next/script';
 
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
@@ -13,11 +15,34 @@ const pretendard = localFont({
   ],
 });
 
+declare global {
+  interface Window {
+    Kakao: any;
+    kakaoSdkLoaded: boolean;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  const handleKakaoScriptLoad = () => {
+    if (typeof window !== 'undefined') {
+      window.kakaoSdkLoaded = true;
+      console.log('Kakao SDK 스크립트 로드 완료!');
+    }
+  };
+
   return (
     <main className={pretendard.className}>
       <Component {...pageProps} />
       <SpeedInsights />
+
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.5/kakao.min.js"
+        integrity="sha384-dok87au0gKqJdxs7msEdBPNnKSRT+/mhTVzq+qOhcL464zXwvcrpjeWvyj1kCdq6"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+        onLoad={handleKakaoScriptLoad} 
+      />
     </main>
   );
 }
+
