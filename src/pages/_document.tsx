8ca10dc9813addbc9d95aaaa078fc6705f69f9d7
sumin <sumin50240585@gmail.com>
2025-07-03 00:29:29 +0900

    ✨ feat :  고유번호 복사 & 카카오 공유 기능 구현

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        {/* 카카오 SDK 스크립트 */}
+        <script
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.5/kakao.min.js"
+    integrity="sha384-dok87au0gKqJdxs7msEdBPNnKSRT+/mhTVzq+qOhcL464zXwvcrpjeWvyj1kCdq6"
+    crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
