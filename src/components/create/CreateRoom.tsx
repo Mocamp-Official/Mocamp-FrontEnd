63dff78ea5bf36e80e375e30eb8e0691c409d2ae
sumin <sumin50240585@gmail.com>
2025-07-05 01:34:17 +0900

    🔧 chore  : 충돌방지

--- src/components/create/CreateRoom.tsx
+++ src/components/create/CreateRoom.tsx
@@ -1,16 +1,11 @@
-import clsx from 'clsx';
-
-import { useRoomForm, RoomFormInput } from '@/hooks/useRoomForm';
-import { createRoom, enterRoom } from '@/apis/room';
-
+import { useState } from 'react';
 import CloseIcon from '@/public/svgs/closeIcon.svg';
-
 import ToggleMicButton from './ToggleMicButton';
 import LabeledBox from './LabeledBox';
 import NumberInput from './NumberInput';
 import ImageUploadBox from './ImageUploadBox';
-import { useRouter } from 'next/navigation';
 import { CreateRoomFormData } from '@/types/create'; 
+
 interface CreateRoomProps {
   formData: CreateRoomFormData;
   setFormData: (data: Partial<CreateRoomFormData>) => void;
@@ -18,147 +13,102 @@ interface CreateRoomProps {
   onClose: () => void;
 }
 
-const CreateRoom = ({ formData, onClose }: CreateRoomProps) => {
-  const {
-  register,
-  values,
-  micOn,
-  toggleMic,
-  errors,
-  isValid,
-  handleSubmit,
-  onImageSelect,
-} = useRoomForm(formData);
+const CreateRoom = ({ onClose }: CreateRoomProps) => {
+  const [micOn, setMicOn] = useState(true);
+  const [roomName, setRoomName] = useState('');
+  const [hour, setHour] = useState('');
+  const [minute, setMinute] = useState('');
+  const [headcount, setHeadcount] = useState('');
+  const [imageFile, setImageFile] = useState<File | null>(null);
 
-  const router = useRouter();
-const onSubmit = async (data: RoomFormInput) => {
-  try {
-    const payload = {
-      roomName: data.roomName,
-      capacity: Number(data.headcount),
-      duration: `${data.hour.padStart(2, '0')}:${data.minute.padStart(2, '0')}`,
-      micAvailability: micOn,
-      micTurnedOn: true,
-      camTurnedOn: true,
-      image: data.imageFile!,
-    };
+  const isFormValid =
+    roomName.trim() !== '' &&
+    (parseInt(hour || '0') > 0 || parseInt(minute || '0') > 0) &&
+    parseInt(headcount || '0') >= 1 &&
+    imageFile !== null;
 
-    const accessToken = localStorage.getItem('accessToken');
-    if (!accessToken) {
-      router.push('/login');
-      return;
-    }
+  const isValidHour = (val: string) => val === '' || (Number(val) >= 0 && Number(val) <= 12);
 
-    const roomId = await createRoom(payload, accessToken);
+  const isValidMinute = (val: string) => val === '' || (Number(val) >= 0 && Number(val) <= 59);
 
-    await enterRoom(roomId, {
-      micTurnedOn: true,
-      camTurnedOn: true,
-    });
+  const isValidHeadcount = (val: string) => val === '' || (Number(val) >= 1 && Number(val) <= 5);
 
-     router.push(`/preview/${roomId}?from=create`);
-  } catch (err) {
-    alert('방 생성에 실패했습니다. 다시 시도해주세요.');
-  }
-};
-
+  const handleMicClick = () => {
+    setMicOn((prev) => !prev);
+  };
 
   return (
     <>
       {/* 방 생성 모달 제목 */}
-      <div className="flex h-5 items-start justify-between lg:h-[29px] xl:h-[38px]">
-        <span className="text-gray9 flex h-full items-center text-lg font-semibold lg:text-2xl xl:text-[32px]">
+      <div className="flex justify-between h-[38px] items-start">
+        <span className="flex items-center text-[32px] leading-[38px] h-full font-semibold text-[#555555]">
           방 생성하기
         </span>
-        <CloseIcon
-          className="h-[13.333px] w-[13.333px] cursor-pointer lg:h-[22.5px] lg:w-[22.5px] xl:h-[29px] xl:w-[29px]"
-          onClick={onClose}
-        />
+        <CloseIcon className="w-[29px] h-[29px] cursor-pointer" onClick={onClose} />
       </div>
 
       {/* 방 이름 + 마이크 설정 */}
-      <div className="mt-[6.67px] flex gap-[10.67px] lg:mt-[8.5px] lg:gap-[15px] xl:mt-3 xl:gap-5">
-        <LabeledBox
-          label="방 이름 *"
-          description={errors.roomName ? errors.roomName.message : '최대 20자'}
-          isError={!!errors.roomName}
-        >
-          <div
-            className={`bg-gray3 flex h-12 w-[186.667px] items-center rounded-[5.333px] px-[21.33px] lg:h-[67.5px] lg:w-[262.5px] lg:rounded-[7.5px] lg:px-7.5 xl:h-[90px] xl:w-[350px] xl:rounded-[10px] xl:px-10 ${
-              errors.roomName ? 'border-red border' : 'border border-transparent'
-            }`}
-          >
+      <div className="flex mt-3 gap-5">
+        <LabeledBox label="방 이름 *" description="최대 20자">
+          <div className="flex items-center w-[350px] h-[90px] bg-[#f2f2f2] px-10 rounded-[10px]">
             <input
-              {...register('roomName')}
-              value={values.roomName}
+              value={roomName}
+              onChange={(e) => {
+                if (e.target.value.length <= 20) {
+                  setRoomName(e.target.value);
+                }
+              }}
               placeholder="방 이름을 설정해주세요"
-              className={`placeholder-gray6 w-full bg-transparent text-[10.67px] font-medium outline-none lg:text-[15px] xl:text-xl ${
-                errors.roomName ? 'text-red' : 'text-gray9'
-              }`}
+              className="w-full bg-transparent outline-none placeholder-[#c4c4c4] text-[20px]"
             />
           </div>
         </LabeledBox>
 
-        <LabeledBox label="마이크 설정">
-          <div className="bg-gray3 flex h-12 w-[101.333px] items-center justify-center rounded-[5.333px] px-[21.33px] py-[12.17px] lg:h-[67.5px] lg:w-[142.5px] lg:rounded-[7.5px] lg:px-7.5 lg:py-[17.25px] xl:h-[90px] xl:w-[190px] xl:rounded-[10px] xl:px-10 xl:py-5">
-            <ToggleMicButton micOn={micOn} onToggle={toggleMic} />
+        <LabeledBox label="마이크 설정 *">
+          <div className="flex items-center justify-center w-[190px] h-[90px] bg-[#f2f2f2] px-10 py-5 rounded-[10px]">
+            <ToggleMicButton micOn={micOn} onToggle={handleMicClick} />
           </div>
         </LabeledBox>
       </div>
 
       {/* 진행 시간 + 인원 수 */}
-      <div className="flex gap-[10.67px] lg:gap-[15px] xl:gap-5">
-        <LabeledBox
-          label="진행 시간 *"
-          description={errors.minute ? errors.minute.message : '최대 12시간'}
-          isError={!!errors.minute}
-        >
-          <div
-            className={`bg-gray3 flex h-12 w-[186.667px] items-center justify-center gap-[5.333px] rounded-[5.333px] px-[21.33px] lg:h-[67.5px] lg:w-[262.5px] lg:gap-[7.5px] lg:rounded-[7.5px] lg:px-7.5 xl:h-[90px] xl:w-[350px] xl:gap-[10px] xl:rounded-[10px] xl:px-10 ${
-              errors.minute ? 'border-red border' : 'border border-transparent'
-            }`}
-          >
+      <div className="flex gap-5">
+        <LabeledBox label="진행 시간 *" description="최대 12시간">
+          <div className="flex justify-center items-center w-[350px] h-[90px] bg-[#f2f2f2] px-10 rounded-[10px] gap-[10px]">
             <NumberInput
               placeholder="00"
-              value={values.hour}
-              maxLength={2}
-              {...register('hour')}
-              containerClassName="max-w-[37.333px] lg:max-w-[52px] xl:max-w-[65px]"
-              inputClassName={errors.minute ? 'text-red' : 'text-primary'}
+              value={hour}
+              onChange={(event) => {
+                const value = event.target.value;
+                if (isValidHour(value)) setHour(value);
+              }}
+              width="max-w-[70px]"
             />
             시간
             <NumberInput
               placeholder="00"
-              value={values.minute}
-              maxLength={2}
-              {...register('minute')}
-              containerClassName="max-w-[37.333px] lg:max-w-[52px] xl:max-w-[65px]"
-              inputClassName={errors.minute ? 'text-red' : 'text-primary'}
+              value={minute}
+              onChange={(event) => {
+                const value = event.target.value;
+                if (isValidMinute(value)) setMinute(value);
+              }}
+              width="max-w-[70px]"
             />
             분
           </div>
         </LabeledBox>
 
-        <LabeledBox
-          label="인원 수 *"
-          description={errors.headcount ? errors.headcount.message : '최대 5명'}
-          isError={!!errors.headcount}
-        >
-          <div
-            className={`bg-gray3 flex h-12 w-[101.333px] items-center justify-center gap-[10px] rounded-[5.333px] px-[21.33px] py-[12.17px] lg:h-[67.5px] lg:w-[142.5px] lg:rounded-[7.5px] lg:px-7.5 lg:py-[17.25px] xl:h-[90px] xl:w-[190px] xl:rounded-[10px] xl:px-10 xl:py-5 ${
-              errors.headcount ? 'border-red border' : 'border border-transparent'
-            }`}
-          >
+        <LabeledBox label="인원 수 *" description="최대 5명">
+          <div className="flex items-center justify-center w-[190px] h-[90px] bg-[#f2f2f2] px-10 py-5 rounded-[10px] gap-[10px]">
             <NumberInput
               placeholder="0"
-              value={values.headcount}
-              maxLength={1}
-              {...register('headcount')}
-              containerClassName="max-w-[29.333px] lg:max-w-[41px] xl:max-w-[55px]"
-              inputClassName={clsx(
-                errors.headcount ? 'text-red' : 'text-primary',
-                'min-w-[8px] lg:min-w-[12px] xl:min-w-[15px]',
-              )}
+              value={headcount}
+              onChange={(event) => {
+                const value = event.target.value;
+                if (isValidHeadcount(value)) setHeadcount(value);
+              }}
+              width="max-w-[55px]"
+              minWidth="min-w-[15px]"
             />
             명
           </div>
@@ -166,20 +116,17 @@ const onSubmit = async (data: RoomFormInput) => {
       </div>
 
       {/* 대표 이미지 설정 */}
-      <ImageUploadBox
-        label="대표 이미지 설정 *"
-        onImageSelect={onImageSelect}
-        errorMessage={errors.imageFile?.message}
-      />
+      <ImageUploadBox label="대표 이미지 설정 *" onImageSelect={(file) => setImageFile(file)} />
 
       {/* 생성 버튼 */}
       <button
-        onClick={handleSubmit(onSubmit)}
-        className={`h-[44.8px] w-[298.667px] shrink-0 rounded-[5.333px] text-[10.67px] font-semibold lg:h-[63px] lg:w-[420px] lg:rounded-[7.5px] lg:text-[15px] xl:h-[84px] xl:w-[560px] xl:rounded-[10px] xl:text-xl ${
-          isValid
-            ? 'bg-primary cursor-pointer text-white'
-            : 'bg-gray3 text-gray6 cursor-not-allowed'
+        className={`w-[560px] h-[84px] shrink-0 text-[20px] font-semibold rounded-[10px] ${
+          isFormValid
+            ? 'bg-[#27CFA5] text-white cursor-pointer'
+            : 'bg-[#f2f2f2] text-[#c4c4c4] cursor-not-allowed'
         }`}
+        
+        disabled={!isFormValid}
       >
         모캠프 생성하기
       </button>
