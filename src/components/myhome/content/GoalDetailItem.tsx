07c021adfbff82e1b34c52624cd49b3f7cc675d3
LimJaeHyun <guswogus81@gmail.com>
2025-07-04 11:22:41 +0900

    ♻️ refactor: 코드 수정

--- src/components/myhome/content/GoalDetailItem.tsx
+++ src/components/myhome/content/GoalDetailItem.tsx
@@ -6,19 +6,15 @@ interface GoalDetailItemProps {
 }
 
 const GoalDetailItem = ({ isCompleted, children }: GoalDetailItemProps) => {
-  if (isCompleted) {
-    return (
-      <div className="flex h-[70px] items-center justify-between rounded-[10px] border border-[#e8e8e8] bg-neutral-50 px-10 py-5">
-        <p className="flex-1 text-xl font-medium text-[#c4c4c4]">{children}</p>
-      </div>
-    );
-  } else {
-    return (
-      <div className="flex h-[70px] items-center justify-between rounded-[10px] border border-[#e8e8e8] bg-white px-10 py-5">
-        <p className="flex-1 text-xl font-medium text-[#555]">{children}</p>
-      </div>
-    );
-  }
+  return isCompleted ? (
+    <div className="flex h-[70px] items-center justify-between rounded-[10px] border border-[#e8e8e8] bg-neutral-50 px-10 py-5">
+      <p className="flex-1 text-xl font-medium text-[#c4c4c4]">{children}</p>
+    </div>
+  ) : (
+    <div className="flex h-[70px] items-center justify-between rounded-[10px] border border-[#e8e8e8] bg-white px-10 py-5">
+      <p className="flex-1 text-xl font-medium text-[#555]">{children}</p>
+    </div>
+  );
 };
 
 export default GoalDetailItem;
