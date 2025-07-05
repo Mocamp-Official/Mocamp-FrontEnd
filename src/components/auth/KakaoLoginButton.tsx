4a95625bfddd7f62a42fad1b276cfed77f3e1c61
chaeyoungwon <epopcy@naver.com>
2025-07-05 18:38:09 +0900

    📱 Design: 로그인 페이지 반응형 적용
     로그인 페이지 반응형 적용

--- src/components/auth/KakaoLoginButton.tsx
+++ src/components/auth/KakaoLoginButton.tsx
@@ -15,16 +15,19 @@ const KakaoLoginButton = () => {
   return (
     <button
       onClick={handleKakaoLogin}
-      className="group relative flex h-[6.25rem] w-full items-center justify-center rounded-[0.3125rem] border border-gray-300 bg-[#FEE500]"
+      className="group relative flex h-[3.333rem] w-full cursor-pointer items-center justify-center rounded-[0.3125rem] border border-gray-300 bg-[#FEE500] lg:h-[4.688rem] xl:h-[6.25rem]"
     >
       <div className="pointer-events-none absolute inset-0 rounded-[0.3125rem] bg-black opacity-0 transition-opacity duration-200 group-hover:opacity-5" />
-      <p className="text-[1.75rem] font-semibold text-[#3C1E1E]">카카오톡 계정으로 시작하기</p>
+      <p className="text-[0.933rem] font-semibold text-[#3C1E1E] lg:text-[1.313rem] xl:text-[1.75rem]">
+        카카오톡 계정으로 시작하기
+      </p>
+
       <div
-        className={`absolute top-[-1.625rem] right-[0.9375rem] items-center justify-center rounded-tl-[0.625rem] rounded-tr-[0.625rem] rounded-br-[0.625rem] border border-[#00af83] bg-white px-5 py-2.5 ${
+        className={`absolute top-[-0.867rem] right-[0.667rem] items-center justify-center rounded-tl-[0.625rem] rounded-tr-[0.625rem] rounded-br-[0.625rem] border border-[#00af83] bg-white px-[0.667rem] py-[0.333rem] lg:top-[-1.219rem] lg:right-[0.969rem] lg:px-[0.938rem] lg:py-[0.469rem] xl:top-[-1.625rem] xl:right-[0.938rem] xl:px-5 xl:py-2.5 ${
           platform === 'kakao' ? 'flex' : 'hidden'
         }`}
       >
-        <p className="flex h-8 w-24 items-center justify-center text-center text-xl font-semibold text-[#00af83]">
+        <p className="flex h-[1.063rem] w-[3rem] items-center justify-center text-center text-[0.667rem] font-semibold tracking-[-0.01875rem] text-[#00af83] lg:h-[1.5rem] lg:w-[4.188rem] lg:text-[0.938rem] xl:h-8 xl:w-24 xl:text-xl">
           최근 로그인
         </p>
       </div>
