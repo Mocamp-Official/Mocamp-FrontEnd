2b8a21ac95a4b748dde98e322a5cea5bc1dbdff2
LimJaeHyun <guswogus81@gmail.com>
2025-07-05 17:33:27 +0900

    ♻️ refactor: 모캠프 사용 추이 API 연결

--- src/components/myhome/content/GoalList.tsx
+++ src/components/myhome/content/GoalList.tsx
@@ -7,11 +7,21 @@ interface GoalListProps {
   goals: GoalDetail[];
 }
 
+// 날짜 형식 변환 함수
+const formatDate = (date: string | null): string | null => {
+  if (!date) return null;
+
+  const [month, day] = date.split('.');
+  return `${month}월 ${day}일`;
+};
+
 const GoalList = ({ selectedDate, goals }: GoalListProps) => {
+  const formattedDate = formatDate(selectedDate);
+
   return (
     <div className="border-gray4 h-[468px] w-[248px] rounded-[10px] border p-5">
-      <p className="text-body1 text-gray9 mt-2.5 mb-1.25">{selectedDate}</p>
-      <p className="text-gray7 mb-6.25">{selectedDate && `목표 달성 수 ${goals.length}개`}</p>
+      <p className="text-body1 text-gray9 mt-2.5 mb-1.25">{formattedDate}</p>
+      <p className="text-gray7 mb-6.25">{formattedDate && `목표 달성 수 ${goals.length}개`}</p>
       <div
         className={`scrollbar-hide max-h-[340px] overflow-y-auto ${
           !selectedDate ? 'flex items-center justify-center' : ''
@@ -19,8 +29,8 @@ const GoalList = ({ selectedDate, goals }: GoalListProps) => {
       >
         {goals.length > 0 ? (
           goals.map((goal) => (
-            <GoalItem key={goal.id} completed={goal.completed}>
-              {goal.title}
+            <GoalItem key={goal.goalId} completed={goal.isCompleted}>
+              {goal.content}
             </GoalItem>
           ))
         ) : selectedDate ? (
@@ -49,12 +59,10 @@ interface GoalItemProps {
 const GoalItem = ({ children, completed = true }: GoalItemProps) => {
   return (
     <div
-      className={`border-gray4 mb-4 flex h-[59px] w-[198px] items-center justify-start rounded-[6px] border pl-5 last:mb-0 ${
-        completed ? 'border-green-200 bg-green-50' : 'bg-gray-50'
-      }`}
+      className={`border-gray4 mb-4 flex h-[59px] w-[198px] items-center justify-start rounded-[6px] border pl-5 last:mb-0`}
     >
       <div className="flex items-center gap-3">
-        <div className={`h-4 w-4 rounded-full ${completed ? 'bg-green-500' : 'bg-gray-300'}`} />
+        <div className={`h-4 w-4 rounded-full`} />
         <p className={`text-base ${completed ? 'text-gray9' : 'text-gray6'}`}>{children}</p>
       </div>
     </div>
