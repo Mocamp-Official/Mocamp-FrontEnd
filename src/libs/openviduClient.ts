94c8cc4a2a830dada17e0147acaca738f196f890
sumin <sumin50240585@gmail.com>
2025-07-10 20:04:07 +0900

    🔧 chore : 빌드 오류 해결

--- src/libs/openviduClient.ts
+++ src/libs/openviduClient.ts
@@ -1,22 +1,9 @@
 import { OpenVidu } from 'openvidu-browser';
 
-// WSS로 배포 -> 배포시 NOT ws
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-const OPENVIDU_WSS = process.env.NEXT_PUBLIC_OPENVIDU_WSS;
-
-if (!BACKEND_URL || !OPENVIDU_WSS) {
-  throw new Error('BACKEND_URL or OPENVIDU_WSS is not defined in env');
-}
-
-export const OPENVIDU_URL = `${BACKEND_URL}:443`;
 let ovInstance: OpenVidu | null = null;
 
 export const initOpenVidu = () => {
   ovInstance = new OpenVidu();
-
-  (OpenVidu as any).websocketUri = OPENVIDU_WSS;
-
-
   return ovInstance;
 };
 
